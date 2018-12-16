899b99a061ab335738a75035d46b3e6e2151c83f
Gergely Szabo <geeryszaabo@gmail.com>
2018-12-17 00:35:45 +0100

    reset

--- bundle/js/localization.js
+++
@@ -1,1 +0,0 @@
-var localization=webpackJsonp_name_([11],{427:function(a,n,o){o(14),a.exports=o(70)}},[427]);
--- js/faq.js
+++
@@ -1,8 +0,0 @@
-﻿$(function () {
-    chrome.browserAction.setPopup({
-        popup: "html/faq.html"
-    });
-    $('.detail-link').click(function () {
-        $(this).next('.detail-content').toggle();
-    });
-});
