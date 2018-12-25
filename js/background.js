6fa69c879463e57e1346df1ee96957287031bf0c
Gergely Szabo <geeryszaabo@gmail.com>
2018-12-26 00:32:35 +0100

    working basic trade unlock timestamp

--- js/background.js
+++ js/background.js
@@ -12,3 +12,77 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
     });
 });
+
+let steamAPIKey = '083D3F215CEFFEE1911D32AC211B2B85';
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.alias !== ""){
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET", 'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + steamAPIKey + '&vanityurl=' + request.alias, true);
+            xhr.onload = function(e) {
+                let steamid = "";
+                if(JSON.parse(xhr.responseText).response.success===1){
+                    steamid = JSON.parse(xhr.responseText).response.steamid;
+                }
+                else{
+                    steamid = request.alias;
+                }
+                let xhr2 = new XMLHttpRequest();
+                xhr2.open("GET", 'https://steamcommunity.com/profiles/' + steamid + '/inventory/json/730/2', true);
+                xhr2.onload = function(e) {
+                    var body=  JSON.parse(xhr2.responseText);
+                    var items = body.rgDescriptions;
+                    var ids = body.rgInventory;
+
+                    let itemsPropertiesToReturn = [];
+
+                    for (let asset in ids) {
+                        var assetid = ids[asset].id;
+                        var position = ids[asset].pos;
+
+                        for (let item in items) {
+                            if(ids[asset].classid===items[item].classid&&ids[asset].instanceid===items[item].instanceid){
+                                let name = items[item].name;
+                                let marketlink = "https://steamcommunity.com/market/listings/730/" + items[item].market_hash_name;
+                                let classid = items[item].classid;
+                                let instanceid = items[item].instanceid;
+                                let tradability = "Tradable";
+
+
+                                if (items[item].tradable === 0) {
+                                    tradability = items[item].cache_expiration;
+                                }
+
+                                if(items[item].marketable === 0){
+                                    tradability = "Not Tradable"
+                                }
+
+                                itemsPropertiesToReturn.push({
+                                    name: name,
+                                    marketlink: marketlink,
+                                    classid: classid,
+                                    instanceid: instanceid,
+                                    assetid: assetid,
+                                    position: position,
+                                    tradability: tradability
+                                })
+                            }
+                        }
+                    }
+
+                    function compare(a,b) {
+                        return a.position - b.position;
+                    }
+
+                    itemsPropertiesToReturn.sort(compare);
+
+                    sendResponse({inventory: itemsPropertiesToReturn});
+
+                };
+                xhr2.send();
+            };
+            xhr.send();
+            return true;
+        }
+    });
--- js/inventory.js
+++ js/inventory.js
@@ -1,21 +1,107 @@
-csDealsButtonText0 ='<a class="btn_small btn_grey_white_innerfade" id="csdeals_inspect0" href="http://csgo.gallery/" target="_blank"><span>Inspect on CS.DEALS...</span></a>';
-csDealsButtonText1 ='<a class="btn_small btn_grey_white_innerfade" id="csdeals_inspect1" href="http://csgo.gallery/" target="_blank"><span>Inspect on CS.DEALS...</span></a>';
-csDealsButtonHtml0 = $.parseHTML(csDealsButtonText0);
-csDealsButtonHtml1 = $.parseHTML(csDealsButtonText1);
+let csDealsButton0 ='<a class="btn_small btn_grey_white_innerfade" id="csdeals_inspect0" href="http://csgo.gallery/" target="_blank"><span>Inspect on CS.DEALS...</span></a>';
+let csDealsButton1 ='<a class="btn_small btn_grey_white_innerfade" id="csdeals_inspect1" href="http://csgo.gallery/" target="_blank"><span>Inspect on CS.DEALS...</span></a>';
+
+let tradable0 = '<div class="descriptor tradability" id="iteminfo0_tradability"><span></span></div>';
+let tradable1 = '<div class="descriptor tradability" id="iteminfo1_tradability"><span></span></div>';
+
+let countdown0 = '<div class="descriptor countdown" id="iteminfo0_countdown"><span></span></div>';
+let countdown1 = '<div class="descriptor countdown" id="iteminfo1_countdown"><span></span></div>';
 
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 let observer = new MutationObserver(function(mutations, observer) {
-    $("#iteminfo1_item_actions").append(csDealsButtonHtml1);
+    let activeID = $(".activeInfo")[0].id.split("730_2_")[1];
+
+    $iteminfo1 = $("#iteminfo1_item_actions");
+    $iteminfo1.append(csDealsButton1);
     inspectLink = $("#iteminfo1_item_actions .btn_small").first().attr("href");
     $("#csdeals_inspect1").attr("href", "http://csgo.gallery/" + inspectLink);
+    if(!$("#iteminfo1_tradability").length){
+        $iteminfo1.after(tradable1);
+    }
 
-    $("#iteminfo0_item_actions").append(csDealsButtonHtml0);
+    $("#iteminfo1_item_owner_descriptors").hide();
+
+    if(/Not Tradable/.test($("#iteminfo1_item_tags_content").html())){
+        $("#iteminfo1_tradability").html("<span class='not_tradable'>Not Tradable</span>");
+        let item = getItemByAssetID(activeID);
+        console.log(item);
+        console.log(item.tradability);
+        console.log(!item);
+
+        if(item){
+            let tradable = new Date(item.tradability);
+            $("#iteminfo1_tradability").html(`<span class='not_tradable'>Tradable After ${tradable}</span>`);
+            if(!$("#iteminfo1_countdown").length){
+                $("#iteminfo1_tradability").after(countdown1);
+            }
+        }
+        else{
+            $("#iteminfo1_tradability").html("<span class='not_tradable'>Not Tradable</span>");
+        }
+    }
+    else{
+        $("#iteminfo1_tradability").html("<span class='tradable'>Tradable</span>");
+    }
+
+
+
+
+
+    $iteminfo0 = $("#iteminfo0_item_actions");
+    $iteminfo0.append(csDealsButton0);
     inspectLink = $("#iteminfo0_item_actions .btn_small").first().attr("href");
     $("#csdeals_inspect0").attr("href", "http://csgo.gallery/" + inspectLink);
+    if(!$("#iteminfo0_tradability").length){
+        $iteminfo0.after(tradable0);
+    }
+
+    $("#iteminfo0_item_owner_descriptors").hide();
+
+    if(/Not Tradable/.test($("#iteminfo0_item_tags_content").html())){
+        let item = getItemByAssetID(activeID);
+        if(item){
+            let tradable = new Date(item.tradability);
+            $("#iteminfo0_tradability").html(`<span class='not_tradable'>Tradable After ${tradable}</span>`);
+            if(!$("#iteminfo0_countdown").length){
+                $("#iteminfo0_tradability").after(countdown0);
+            }
+        }
+        else{
+            $("#iteminfo0_tradability").html("<span class='not_tradable'>Not Tradable</span>");
+        }
+
+    }
+    else{
+        $("#iteminfo0_tradability").html("<span class='tradable'>Tradable</span>");
+    }
 });
 
 observer.observe(document.getElementById("iteminfo0"), {
     subtree: false,
     attributes: true
 });
+
+
+let alias = window.location.href;
+
+if(/\/id\//.test(window.location.href)){
+    alias = window.location.href.split("/id/")[1].split("/inventory")[0];
+}
+else{
+    alias = window.location.href.split("/profiles/")[1].split("/inventory")[0];
+}
+
+var items = [];
+
+chrome.runtime.sendMessage({alias: alias}, function(response) {
+    items = response.inventory;
+    //console.log(new Date(items[0].tradability));
+});
+
+function getItemByAssetID(assetidToFind){
+    if (items === undefined || items.length === 0) {
+        return false
+    }
+    return $.grep(items, function(e){ return e.assetid === assetidToFind; })[0];
+}
