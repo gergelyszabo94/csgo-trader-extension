6777a6501ef16a3e81e389a3e42efc5ac590da98
Gergely Szabo <geeryszaabo@gmail.com>
2019-01-01 18:15:16 +0100

    removed all getting started boilerplate leftover

--- js/background.js
+++ js/background.js
@@ -1,18 +1,3 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
-
 let steamID = "";
 
 //sets steamid for the future api request and redirect the inventory call to load full inventory
