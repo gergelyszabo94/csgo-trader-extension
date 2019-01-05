5340ac21dd0540f01c91bfcd00a0f143560d981b
Gergely Szabo <geeryszaabo@gmail.com>
2019-01-06 00:03:37 +0100

    version 1.2 - inspect on csdeals button in trade offers
    inspect on csdeals button in trade offers

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The features I develop will be complementary to Steam Inventory Helper for now.
 
 Current Features:
 --------------
-- Adds an "Inspect on CS.DEALS..." button beside the default inspect button in inventories
+- Adds an "Inspect on CS.DEALS..." button beside the default inspect button in inventories and trade offers
 - Shows when an item's tradelock will expire in the browser's timezone
 - Shows a countdown that counts down to the date when the items will become tradable
 - Puts a small indicator to each item in the inventory of when they will become tradable
---
+++ js/tradeoffer.js
@@ -0,0 +1,25 @@
+let csdealsButton = '<a class="popup_menu_item" id="csdeals_inspect_button" href="http://csgo.gallery/" target="_blank">Inspect on CS.DEALS...</a>';
+
+$("#trade_action_popup_itemactions").after(csdealsButton);
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+let observer = new MutationObserver(function(mutations, observer) {
+    if($("#csdeals_inspect_button").attr("href")==="http://csgo.gallery/"){
+        addClickListener();
+        console.log("mutation");
+    }
+});
+
+observer.observe(document.getElementById("inventories"), {
+    subtree: true,
+    attributes: true
+});
+
+function addClickListener(){
+    $(".slot_actionmenu_button").on("click", function () {
+        console.log("clicked");
+        let inspectLink = $("#trade_action_popup_itemactions").find("a.popup_menu_item").first().attr("href");
+        $("#csdeals_inspect_button").attr("href", "http://csgo.gallery/" + inspectLink);
+    });
+}
