19a5ca6fe1312b94cbfa961b2c06c8603db691c3
Gergely Szabo <geeryszaabo@gmail.com>
2019-02-03 14:58:04 +0100

    profile.js only runs on profiles

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,6 @@
-//console.log("profile");
-
-// let repButton = `<span class="btn_green_white_innerfade btn_small" id="repper">+rep<span>`;
-//
-// $(".commentthread_entry_submitlink").append(repButton);
+//ensures that we are on a profile page, it's not possible with simple regex
+if($("body").hasClass("profile_page")){
+    let repButton = `<span class="btn_green_white_innerfade btn_small" id="repper">+rep<span>`;
+    // $(".commentthread_entry_submitlink").append(repButton);
+    console.log("we are on a profile");
+}
