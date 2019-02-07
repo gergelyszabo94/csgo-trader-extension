b4d72dd48c1bc545618fb8f60b265fa14910ee94
Gergely Szabo <geeryszaabo@gmail.com>
2019-02-07 23:55:24 +0100

    background throttling hack improvement

--- js/tradeoffers.js
+++ js/tradeoffers.js
@@ -4,7 +4,19 @@ overrideShowTradeOffer();
 //chrome background tab throttling causes steam's own js files to load later than the these injections, so it does not override the functions
 //this only happens when the tab is opened in the background, https://www.chromestatus.com/feature/5527160148197376
 //this is a dirty but working fix for that
-setTimeout(function () {
-    overrideShowTradeOffer();
-    overrideDecline();
-}, 10000);
+let thisManyTimes = 15;
+(function () {
+    let id = setInterval(function(){
+        if(thisManyTimes>0){
+            overrideShowTradeOffer();
+            overrideDecline();
+        }
+        else{
+            clearInterval(id);
+        }
+        thisManyTimes--;
+        console.log(thisManyTimes + " executed");
+    }, 1000);
+})();
+
+
