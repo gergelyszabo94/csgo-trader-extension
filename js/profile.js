1984db163a6cf1da81b471f463e616c636742323
Gergely Szabo <geeryszaabo@gmail.com>
2019-02-08 00:15:14 +0100

    +rep button init

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,19 @@
 //ensures that we are on a profile page, it's not possible with simple regex
-if($("body").hasClass("profile_page")){
-    let repButton = `<span class="btn_green_white_innerfade btn_small" id="repper">+rep<span>`;
-    // $(".commentthread_entry_submitlink").append(repButton);
-
-    overrideShowTradeOffer();
-}
+// if($("body").hasClass("profile_page")){
+//     let repText = "repreprep";
+//     let repButton = `<span class="btn_green_white_innerfade btn_small" id="repper">+rep<span>`;
+//     $(".commentthread_textarea").after(repButton);
+//     $("#repper").click(function () {
+//         $(".commentthread_textarea").text(repText);
+//        $("form").each(function () {
+//            console.log($(this));
+//            console.log($(this).attr('id'));
+//            if(/commentthread/.test($(this).attr('id'))){
+//                console.log("in the if");
+//                $(this).submit();
+//            }
+//        })
+//     });
+//
+//     overrideShowTradeOffer();
+// }
