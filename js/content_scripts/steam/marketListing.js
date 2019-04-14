9451c8918bba9e31c7d51b0bd168f8b1838e8cce
Gergely Szabo <geeryszaabo@gmail.com>
2019-04-14 17:18:13 +0200

    float bar on market pages

--- js/content_scripts/steam/marketListing.js
+++ js/content_scripts/steam/marketListing.js
@@ -13,6 +13,7 @@ const exteriors = `
 
 const inBrowserInspectButton =`<a class="btn_small btn_grey_white_innerfade" id="inbrowser_inspect_button" href="http://csgo.gallery/" target="_blank"><span>${chrome.i18n.getMessage("inspect_in_browser")}</span></a>`;
 const inBrowserInspectButtonPopupLink = `<a class="popup_menu_item" id="inbrowser_inspect" href="http://csgo.gallery/" target="_blank">${chrome.i18n.getMessage("inspect_in_browser")}</a>`;
+const getFloatInfoMenuItem = `<span class="popup_menu_item" id="get_float">${chrome.i18n.getMessage("get_float_info")}</span>`;
 
 
 const dopplerPhase = "<div class='dopplerPhaseMarket'><span></span></div>";
@@ -92,7 +93,7 @@ else{
 }
 
 $("#largeiteminfo_item_actions").append(inBrowserInspectButton);
-$("#market_action_popup_itemactions").after(inBrowserInspectButtonPopupLink);
+$("#market_action_popup_itemactions").after(inBrowserInspectButtonPopupLink, getFloatInfoMenuItem);
 
 let inspectLink = $("#largeiteminfo_item_actions").find(".btn_small.btn_grey_white_innerfade").first().attr("href");
 $("#inbrowser_inspect_button").attr("href", "http://csgo.gallery/" + inspectLink);
@@ -100,6 +101,93 @@ $("#inbrowser_inspect_button").attr("href", "http://csgo.gallery/" + inspectLink
 $("#inbrowser_inspect").hover(function () {
     let inspectLink = $("#market_action_popup_itemactions").find("a.popup_menu_item").first().attr("href");
     $("#inbrowser_inspect").attr("href", "http://csgo.gallery/" + inspectLink);
+});
+
+$("#get_float").hover(function () {
+    let inspectLink = $("#market_action_popup_itemactions").find("a.popup_menu_item").first().attr("href");
+    let listingID = inspectLink.split("preview%20M")[1].split("A")[0];
+    $("#get_float").attr({"data-listing-id": listingID, "inspect-link": inspectLink});
+});
+
+
+$("#get_float").click(function () {
+    let listingID = $(this).attr("data-listing-id");
+    let inspectLink = $(this).attr("inspect-link");
+
+    $(".popup_body.popup_menu").hide();
+
+    chrome.runtime.sendMessage({getFloatInfo: inspectLink}, function(response) {
+        try{
+            float = response.floatInfo.floatvalue;
+            paintIndex = response.floatInfo.paintindex;
+            paintSeed = response.floatInfo.paintseed;
+            origin = response.floatInfo.origin_name;
+            min = response.floatInfo.min;
+            max = response.floatInfo.max;
+            stickers = response.floatInfo.stickers;
+
+        }
+        catch{
+
+        }
+
+        let position = float.toFixed(2)*100-2;
+
+        let floatBar = `
+<div class="floatBarMarket">
+    <div class="floatToolTip" style="left: ${position}%">
+        <div>Float: <span id="float1DropTarget">${float.toFixed(4)}</span></div>
+        <svg id="floatPointer" class="floatPointer" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>
+   </div>
+ <div class="progress">
+    <div class="progress-bar floatBarFN" title="${chrome.i18n.getMessage("fn_long")}"></div>
+    <div class="progress-bar floatBarMW" title="${chrome.i18n.getMessage("mw_long")}"></div>
+    <div class="progress-bar floatBarFT" title="${chrome.i18n.getMessage("ft_long")}"></div>
+    <div class="progress-bar floatBarWW" title="${chrome.i18n.getMessage("ww_long")}"></div>
+     <div class="progress-bar floatBarBS" title="${chrome.i18n.getMessage("bs_long")}"></div>
+ </div>
+  <div class="showTechnical">Show Technical</div>
+ <div class="floatTechnical">
+        Technical:<br>
+        Float Value: ${float}<br>
+        Paint Index: ${paintIndex}<br>
+        Paint Seed: ${paintSeed}<br>
+        Origin: ${origin}<br>
+        Best Possible Float: ${min}<br>
+        Worst Possible Float: ${max}<br>
+        <br>
+        Float info from <a href="https://csgofloat.com/" target="_blank">csgofloat.com</a>
+</div>
+</div>`;
+
+        // //sticker wear to sticker icon tooltip
+        // stickers.forEach(function (stickerInfo, index) {
+        //     let wear = 100;
+        //     if(stickerInfo.wear!==null){
+        //         wear =  Math.trunc(stickerInfo.wear*100);
+        //     }
+        //     $currentSticker1 = $("#stickers1").find($(".stickerSlot")).eq(index);
+        //     $currentSticker0 = $("#stickers0").find($(".stickerSlot")).eq(index);
+        //     $currentSticker1.attr("data-tooltip", stickerInfo.name + " - Condition: " + wear + "%");
+        //     $currentSticker0.attr("data-tooltip", stickerInfo.name + " - Condition: " + wear + "%");
+        //     $currentSticker1.find("img").css("opacity", wear/100);
+        //     $currentSticker0.find("img").css("opacity", wear/100);
+        // });
+        //
+
+        if(float===0){
+            $("#listing_"+listingID+"_name").parent().append("<div>Could not get Float from csgofloat.com</div>");
+        }
+        else{
+            $("#listing_"+listingID+"_name").parent().append(floatBar);
+        }
+
+
+        $(".showTechnical").click(function () {
+            $(".floatTechnical").toggle();
+        });
+    });
+
 });
 
 if(/Doppler/.test(window.location.href)){
