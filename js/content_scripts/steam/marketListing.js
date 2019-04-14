2096df9a4c4242ad86ca1338708129558d7233f4
Gergely Szabo <geeryszaabo@gmail.com>
2019-04-14 14:49:44 +0200

    market listings localized

--- js/content_scripts/steam/marketListing.js
+++ js/content_scripts/steam/marketListing.js
@@ -1,18 +1,19 @@
 const exteriors = `
     <div class="descriptor otherExteriors" id="otherExteriors">
-        <span>Links to other exteriors:</span>
+        <span>${chrome.i18n.getMessage("links_to_other_exteriors")}:</span>
         <ul>
-            <li><a href="" target="_blank" id="fnLink">Factory New</a> - <a href="" target="_blank" id="fnSTLink"><span class="stattrakOrange">StatTrak™ Factory New</span></a></li>
-            <li><a href="" target="_blank" id="mwLink">Minimal Wear</a> - <a href="" target="_blank" id="mwSTLink"><span class="stattrakOrange">StatTrak™ Minimal Wear</span></a></li>
-            <li><a href="" target="_blank" id="ftLink">Field-Tested</a> - <a href="" target="_blank" id="ftSTLink"><span class="stattrakOrange">StatTrak™ Field-Tested</span></a></li>
-            <li><a href="" target="_blank" id="wwLink">Well-Worn</a> - <a href="" target="_blank" id="wwSTLink"><span class="stattrakOrange">StatTrak™ Well-Worn</span></a></li>
-            <li><a href="" target="_blank" id="bsLink">Battle-Scarred</a> - <a href="" target="_blank" id="bsSTLink"><span class="stattrakOrange">StatTrak™ Battle-Scarred</span></a></li>
+            <li><a href="" target="_blank" id="fnLink">${chrome.i18n.getMessage("fn_long")}</a> - <a href="" target="_blank" id="fnSTLink"><span class="stattrakOrange">StatTrak™ ${chrome.i18n.getMessage("fn_long")}</span></a></li>
+            <li><a href="" target="_blank" id="mwLink">${chrome.i18n.getMessage("mw_long")}</a> - <a href="" target="_blank" id="mwSTLink"><span class="stattrakOrange">StatTrak™ ${chrome.i18n.getMessage("mw_long")}</span></a></li>
+            <li><a href="" target="_blank" id="ftLink">${chrome.i18n.getMessage("ft_long")}</a> - <a href="" target="_blank" id="ftSTLink"><span class="stattrakOrange">StatTrak™ ${chrome.i18n.getMessage("ft_long")}</span></a></li>
+            <li><a href="" target="_blank" id="wwLink">${chrome.i18n.getMessage("ww_long")}</a> - <a href="" target="_blank" id="wwSTLink"><span class="stattrakOrange">StatTrak™ ${chrome.i18n.getMessage("ww_long")}</span></a></li>
+            <li><a href="" target="_blank" id="bsLink">${chrome.i18n.getMessage("bs_long")}</a> - <a href="" target="_blank" id="bsSTLink"><span class="stattrakOrange">StatTrak™ ${chrome.i18n.getMessage("bs_long")}</span></a></li>
         </ul>
-        <span>Not every item is available in every exterior</span>
+        <span>${chrome.i18n.getMessage("not_every_available")}</span>
     </div>`;
 
-const inBrowserInspectButton ='<a class="btn_small btn_grey_white_innerfade" id="inbrowser_inspect_button" href="http://csgo.gallery/" target="_blank"><span>Inspect in Browser...</span></a>';
-const inBrowserInspectButtonPopupLink = '<a class="popup_menu_item" id="inbrowser_inspect" href="http://csgo.gallery/" target="_blank">Inspect in Browser...</a>';
+const inBrowserInspectButton =`<a class="btn_small btn_grey_white_innerfade" id="inbrowser_inspect_button" href="http://csgo.gallery/" target="_blank"><span>${chrome.i18n.getMessage("inspect_in_browser")}</span></a>`;
+const inBrowserInspectButtonPopupLink = `<a class="popup_menu_item" id="inbrowser_inspect" href="http://csgo.gallery/" target="_blank">${chrome.i18n.getMessage("inspect_in_browser")}</a>`;
+
 
 const dopplerPhase = "<div class='dopplerPhaseMarket'><span></span></div>";
 
@@ -64,23 +65,23 @@ if(isSouvenir||thereSouvenirForThisItem){
 
     $fnst=$("#fnSTLink");
     $fnst.attr("href", genericMarketLink + souvenir + weaponName + "%28Factory%20New%29");
-    $fnst.find("span").text("Souvenir Factory New");
+    $fnst.find("span").text(chrome.i18n.getMessage("souvenir")+" " + chrome.i18n.getMessage("fn_long"));
 
     $mwst=$("#mwSTLink");
     $mwst.attr("href", genericMarketLink + souvenir + weaponName + "%28Minimal%20Wear%29");
-    $mwst.find("span").text("Souvenir Minimal Wear");
+    $mwst.find("span").text(chrome.i18n.getMessage("souvenir")+" " + chrome.i18n.getMessage("mw_long"));
 
     $ftst=$("#ftSTLink");
     $ftst.attr("href", genericMarketLink + souvenir + weaponName + "%28Field-Tested%29");
-    $ftst.find("span").text("Souvenir Field-Tested");
+    $ftst.find("span").text(chrome.i18n.getMessage("souvenir")+" " + chrome.i18n.getMessage("ft_long"));
 
     $wwst=$("#wwSTLink");
     $wwst.attr("href", genericMarketLink + souvenir + weaponName + "%28Well-Worn%29");
-    $wwst.find("span").text("Souvenir Well-Worn");
+    $wwst.find("span").text(chrome.i18n.getMessage("souvenir")+" " + chrome.i18n.getMessage("ww_long"));
 
     $bsst=$("#bsSTLink");
     $bsst.attr("href", genericMarketLink + souvenir + weaponName + "%28Battle-Scarred%29");
-    $bsst.find("span").text("Souvenir Battle-Scarred");
+    $bsst.find("span").text(chrome.i18n.getMessage("souvenir")+" " + chrome.i18n.getMessage("bs_long"));
 }
 else{
     $("#fnSTLink").attr("href", genericMarketLink + star + stattrak + weaponName + "%28Factory%20New%29");
@@ -97,7 +98,6 @@ let inspectLink = $("#largeiteminfo_item_actions").find(".btn_small.btn_grey_whi
 $("#inbrowser_inspect_button").attr("href", "http://csgo.gallery/" + inspectLink);
 
 $("#inbrowser_inspect").hover(function () {
-    console.log("hovered");
     let inspectLink = $("#market_action_popup_itemactions").find("a.popup_menu_item").first().attr("href");
     $("#inbrowser_inspect").attr("href", "http://csgo.gallery/" + inspectLink);
 });
