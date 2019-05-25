5892000c42c81c87465e3d203f31a27ed095eafc
Gergely Szabo <geeryszaabo@gmail.com>
2019-05-25 12:09:31 +0200

    adds reporting to sharedfiles, replies to to group announcements, closes #40

--- js/content_scripts/steam/group.js
+++ js/content_scripts/steam/group.js
@@ -1,2 +1,9 @@
 addReplytoCommentsFunctionality();
 addCommentsMutationObserver();
+reportComments();
+
+setInterval(function () {
+    if(/#announcements|#comments/.test(window.location.href)){
+        addReplytoCommentsFunctionality();
+    }
+},2000);
--- js/content_scripts/steam/sharedfile.js
+++ js/content_scripts/steam/sharedfile.js
@@ -1,2 +1,3 @@
 addReplytoCommentsFunctionality();
 addCommentsMutationObserver();
+reportComments();
