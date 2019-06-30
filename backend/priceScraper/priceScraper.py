41f6b87c28a6e567d999b3339a8124beb0a1d968
Gergely Szabo <geeryszaabo@gmail.com>
2019-06-30 17:11:47 +0200

    csgobackpack extract local

--- backend/priceScraper/priceScraper.py
+++ backend/priceScraper/priceScraper.py
@@ -1,7 +1,30 @@
 import json
+import requests
+
+response = requests.get("http://csgobackpack.net/api/GetItemsList/v2/")
+responseJSON = response.json()
+
+if response.status_code == 200 and responseJSON['success']:
+    items = responseJSON['items_list']
+    extract = {}
+    for key, value in items.items():
+        name = items.get(key).get('name')
+        price = items.get(key).get('price')
+
+        if price:
+            extract[name] = {}
+            extract[name]['csgobackpack'] = price
+        else:
+            extract[name] = {}
+            extract[name]['csgobackpack'] = "null"
+
+    print(extract)
+else:
+    print("Request failed with status code: ", response.status_code)
+
 
 def lambda_handler(event, context):
-    # TODO implement
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
