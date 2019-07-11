63d3ada20e3d91ff6759f40dd435f21b4cc12ac3
Gergely Szabo <geeryszaabo@gmail.com>
2019-07-11 17:19:01 +0200

    added exchange rates scraping #8

---
+++ backend/exchangeRates/exchangeRates.py
@@ -0,0 +1,71 @@
+import json
+import requests
+import boto3
+import os
+from datetime import date
+
+result_s3_bucket = os.environ['RESULTS_BUCKET']
+sns_topic = os.environ['SNS_TOPIC_ARN']
+
+
+def lambda_handler(event, context):
+    print("Requesting exchange rates from exchangeratesapi.io")
+    try:
+        response = requests.get("https://api.exchangeratesapi.io/latest?base=USD")
+    except Exception as e:
+            print(e)
+            error = "Error during request"
+            alert_via_sns(f'{error}: {e}')
+            return {
+                'statusCode': 500,
+                'body': error
+            }
+    print("Response from exchangeratesapi.io")
+    try:
+        rates = response.json()['rates']
+    except Exception as e:
+        print(e)
+        error = "Error parsing rates from the request response"
+        alert_via_sns(f'{error}: {e}')
+        return {
+            'statusCode': 500,
+            'body': error
+        }
+    push_to_s3(rates)
+    return {
+        'statusCode': 200,
+        'body': "Success"
+    }
+
+
+def push_to_s3(content):
+    print("Getting date for result path")
+
+    today = date.today()
+    year = today.strftime("%Y")
+    month = today.strftime("%m")
+    day = today.strftime("%d")
+
+    s3 = boto3.resource('s3')
+
+    print("Updating exchange_rates.json in s3")
+    s3.Object(result_s3_bucket, 'exchange_rates.json').put(
+        Body=(bytes(json.dumps(content, indent=2).encode('UTF-8')))
+    )
+    print("exchange_rates.json updated")
+    print(f'Uploading prices to {year}/{month}/{day}/exchange_rates.json')
+    s3.Object(result_s3_bucket, f'{year}/{month}/{day}/exchange_rates.json').put(
+        Body=(bytes(json.dumps(content, indent=2).encode('UTF-8')))
+    )
+    print("Upload complete")
+
+
+def alert_via_sns(error):
+    print("Publishing error to SNS")
+
+    sns = boto3.client('sns')
+
+    response = sns.publish(
+        TopicArn=sns_topic,
+        Message=f'Failed to update exchange rates, error: {error}',
+    )
