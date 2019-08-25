d90233bdb03c873510ea8950a9c54438db40588d
Gergely Szabo <geeryszaabo@gmail.com>
2019-08-25 21:53:07 +0200

    refactor to vanilla js and to new code style

--- extension/chrome/js/content_scripts/steam/profile.js
+++ extension/chrome/js/content_scripts/steam/profile.js
@@ -1,105 +1,105 @@
-//ensures that we are on a profile page, it's not possible with simple regex
-if($("body").hasClass("profile_page")){
+// ensures that we are on a profile page, it's not possible with simple regex
+if (document.querySelector('body').classList.contains('profile_page')){
     updateLoggedInUserID();
     const profileOwnerSteamID = getProfileOwnerSteamID();
-    const copyPermalink = `<a class="popup_menu_item" href="#" id="copy_profile_perma_link"><img style="width: 16px; height: 16px" src="${chrome.runtime.getURL("images/paperclip.png")}">&nbsp; Copy Profile Permalink</a>`;
-    const textareaToCopy = `<textarea id="text_area_to_copy_permalink" style="position: absolute; left: -9999px" readonly="">https://steamcommunity.com/profiles/${profileOwnerSteamID}</textarea>`;
-    $("#profile_action_dropdown").find(".popup_body.popup_menu.shadow_content").append(copyPermalink);
-
-    $("#copy_profile_perma_link").click(function () {
-        $("body").append(textareaToCopy);
-        $("#text_area_to_copy_permalink").select();
-        document.execCommand('copy');
-        $("#text_area_to_copy_permalink").remove();
-    });
+    const loggedInUserID =  getUserSteamID();
 
     addReplytoCommentsFunctionality();
     addCommentsMutationObserver();
 
-    chrome.storage.local.get(['markScammers'], function(result) {
-        if(result.markScammers){
-            warnOfScammer(profileOwnerSteamID, "profile");
-        }
-    });
+    // changes background and adds a banner if steamrep banned scammer detected
+    chrome.storage.local.get('markScammers', result => {if(result.markScammers) warnOfScammer(profileOwnerSteamID, 'profile')});
 
-    if(getUserSteamID()===profileOwnerSteamID){ //when on the logged in user's own profile
-        chrome.storage.local.get(['reoccuringMessage', 'showReoccButton'], function(result) {
+    // resizes the elements where the rep or reoccuring buttons will be inserted
+    let commentThreadEntryBox = document.querySelector('.commentthread_entry_quotebox');
+    if (commentThreadEntryBox !== null) commentThreadEntryBox.setAttribute('style', 'width: 83%; display: inline-block;');
+
+    if (loggedInUserID === profileOwnerSteamID){ //when on the logged in user's own profile
+        chrome.storage.local.get(['reoccuringMessage', 'showReoccButton'], (result) => {
             if(result.showReoccButton){
-                let reooccText = result.reoccuringMessage;
                 let reooccButton = `<div style="float: right; text-align: center; margin-top: 6px;" class="commentthread_user_avatar playerAvatar"><span class="btn_green_white_innerfade btn_small" id="reocc" style="padding: 5px;">Reocc<span></div>`;
 
-                $commentthreadentrybox = $(".commentthread_entry_quotebox");
-                $commentthreadentrybox.css({"width": "83%", "display":"inline-block"});
-                $commentthreadentrybox.after(reooccButton);
+                commentThreadEntryBox.insertAdjacentHTML('afterend', reooccButton);
 
-                $("#reocc").click(function () {
-                    $(".commentthread_comment.responsive_body_text").each(function(){
-                        $commentthread = $(this);
-                        if($commentthread.find(".commentthread_comment_text").text().replace(/\s/g,'')===result.reoccuringMessage.replace(/\s/g,'')){
-                            $commentthread.find("img")[1].click();
-                        }
+                document.getElementById('reocc').addEventListener('click', () => {
+                   document.querySelectorAll('.commentthread_comment.responsive_body_text').forEach(commentThread => {
+                       // regex: replaces whitespaces
+                       if (commentThread.querySelector('.commentthread_comment_text').innerText.replace(/\s/g,'') === result.reoccuringMessage.replace(/\s/g,'')){
+                           commentThread.querySelectorAll('img')[1].click();
+                       }
                     });
-                    $(".commentthread_textarea").val(reooccText);
-                    setTimeout(function(){
-                        $(".btn_green_white_innerfade.btn_small")[1].click();
-                    }, 2000);
+                   document.querySelector('.commentthread_textarea').value = result.reoccuringMessage;
+                   setTimeout(() => {document.querySelectorAll('.btn_green_white_innerfade.btn_small')[1].click()}, 2000);
+
                 });
             }
         });
     }
     else{ //when on someone else's profile
-        chrome.storage.local.get(['reputationMessage', 'showPlusRepButton'], function(result) {
+        // adds "copy profile permalink" to the context menu
+        const copyPermalink = `<a class="popup_menu_item" href="#" id="copy_profile_perma_link"><img style="width: 16px; height: 16px" src="${chrome.runtime.getURL("images/paperclip.png")}">&nbsp; Copy Profile Permalink</a>`;
+        document.getElementById('profile_action_dropdown').querySelector('.popup_body.popup_menu.shadow_content').insertAdjacentHTML('beforeend', copyPermalink);
+
+        // this is a workaround to only being able to copy text to the clipboard that is selected in a textbox
+        const textareaToCopy = `<textarea id="text_area_to_copy_permalink" style="position: absolute; left: -9999px" readonly="">https://steamcommunity.com/profiles/${profileOwnerSteamID}</textarea>`;
+
+        document.getElementById('copy_profile_perma_link').addEventListener('click', () => {
+            document.querySelector('body').insertAdjacentHTML('beforeend', textareaToCopy);
+            let textAreaElement = document.getElementById('text_area_to_copy_permalink');
+            textAreaElement.select();
+            document.execCommand('copy');
+            textAreaElement.parentNode.removeChild(textAreaElement);
+        });
+
+        // handles rep button related stuff
+        chrome.storage.local.get(['reputationMessage', 'showPlusRepButton'], (result) => {
             if(result.showPlusRepButton){
-                let repText =result.reputationMessage;
                 let repButton = `<div style="float: right; text-align: center; margin-top: 6px;" class="commentthread_user_avatar playerAvatar"><span class="btn_green_white_innerfade btn_small" id="repper" style="padding: 5px;">+rep<span></div>`;
 
-                $commentthreadentrybox = $(".commentthread_entry_quotebox");
-                $commentthreadentrybox.css({"width": "83%", "display":"inline-block"});
-                $commentthreadentrybox.after(repButton);
+                if (commentThreadEntryBox !== null) commentThreadEntryBox.insertAdjacentHTML('afterend', repButton);
 
-                $("#repper").click(function () {
-                    $(".commentthread_textarea").val(repText);
-                    setTimeout(function(){
-                        $(".btn_green_white_innerfade.btn_small")[1].click();
-                    }, 500);
+                document.getElementById('repper').addEventListener('click', () => {
+                    document.querySelector('.commentthread_textarea').value = result.reputationMessage;
+                    setTimeout(() => {document.querySelectorAll('.btn_green_white_innerfade.btn_small')[1].click()}, 500);
+
                 });
             }
             reportComments();
         });
     }
 
-    chrome.storage.local.get(['nsfwFilter'], function(result) {
+    chrome.storage.local.get('nsfwFilter', (result) => {
         if(result.nsfwFilter){
-            //makes the profile background the same as the default one
-            $(".no_header.profile_page").css({"background-image": "url(https://steamcommunity-a.akamaihd.net/public/images/profile/profile_bg.jpg)", "background-repeat": "repeat-x", "background-color": "#262627"});
-            $(".no_header.profile_page").removeClass("has_profile_background ");
-            $(".profile_content").removeClass("has_profile_background ");
-            $("body").removeClass("has_profile_background ");
-            $(".profile_background_holder_content").remove();
+            // makes the profile background the same as the default one
+            document.querySelector('.no_header.profile_page').setAttribute('style', 'background-image: url(https://steamcommunity-a.akamaihd.net/public/images/profile/profile_bg.jpg); background-repeat: repeat-x; background-color: #262627;');
+            document.querySelectorAll('.profile_content, body, .no_header.profile_page').forEach(element => {element.classList.remove('has_profile_background')});
 
-            //hides artwork and screenshot showcases
-            $(".screenshot_showcase").hide();
+            // removes artwork and screenshot showcases
+            document.querySelectorAll('.profile_background_holder_content, .screenshot_showcase').forEach(element => {element.parentNode.removeChild(element)});
 
-            //changes avatar to the default one
-            $(".playerAvatarAutoSizeInner").find("img").attr("src", "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg");
+            // changes avatar to the default one
+            document.querySelector('.playerAvatarAutoSizeInner').querySelector('img').setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg');
         }
     });
     overrideShowTradeOffer();
 
-    chrome.storage.local.get(['showRealStatus'], function(result) {
+    // shows actual steam chat status if set in options
+    chrome.storage.local.get('showRealStatus', (result) => {
         if(result.showRealStatus){
-            $statusDiv = $(".profile_in_game.persona");
-            if($statusDiv.hasClass("online")){
-                $textDiv = $statusDiv.children(".profile_in_game_header");
-                chrome.runtime.sendMessage({GetPlayerSummaries: profileOwnerSteamID}, function(response) {
+            let statusDiv = document.querySelector('.profile_in_game.persona');
+
+            if (statusDiv.classList.contains('online')){
+                let textDiv = statusDiv.querySelector('.profile_in_game_header');
+
+                chrome.runtime.sendMessage({GetPlayerSummaries: profileOwnerSteamID}, (response) => {
                     if(response.apiKeyValid){
                         switch(response.personastate){
                             case 1: break;
-                            case 2: $textDiv.text("Currently Busy"); break;
-                            case 3: $textDiv.text("Currently Away"); break; //It looks like the other ones might not really exist anymore
-                            case 4: $textDiv.text("Currently Snooze"); break;
-                            case 5: $textDiv.text("Currently Looking to Trade"); break;
-                            case 6: $textDiv.text("Currently Looking to Play"); break;
+                            case 2: textDiv.innerText = ("Currently Busy"); break;
+                            case 3: textDiv.innerText = ("Currently Away"); break;
+                            case 4: textDiv.innerText = ("Currently Snooze"); break;
+                            case 5: textDiv.innerText = ("Currently Looking to Trade"); break;
+                            case 6: textDiv.innerText = ("Currently Looking to Play"); break;
                         }
                     }
                 });
