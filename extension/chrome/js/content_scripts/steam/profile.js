9279c39480a0b4d398d9b479cede1dbd2f925a14
Gergely Szabo <geeryszaabo@gmail.com>
2019-08-27 10:40:27 +0200

    handles private profiles

--- extension/chrome/js/content_scripts/steam/profile.js
+++ extension/chrome/js/content_scripts/steam/profile.js
@@ -4,6 +4,10 @@ if (document.querySelector('body').classList.contains('profile_page')){
     const profileOwnerSteamID = getProfileOwnerSteamID();
     const loggedInUserID =  getUserSteamID();
 
+    let isProfilePrivate = false;
+    let profileActionPopup = document.getElementById('profile_action_dropdown');
+    if (profileActionPopup === null) isProfilePrivate = true;
+
     addReplytoCommentsFunctionality();
     addCommentsMutationObserver();
 
@@ -42,37 +46,39 @@ if (document.querySelector('body').classList.contains('profile_page')){
             }
         });
     }
-    else{ //when on someone else's profile
-        // adds "copy profile permalink" to the context menu
-        const copyPermalink = `<a class="popup_menu_item" href="#" id="copy_profile_perma_link"><img style="width: 16px; height: 16px" src="${chrome.runtime.getURL("images/paperclip.png")}">&nbsp; Copy Profile Permalink</a>`;
-        document.getElementById('profile_action_dropdown').querySelector('.popup_body.popup_menu.shadow_content').insertAdjacentHTML('beforeend', copyPermalink);
+    else{ // when on someone else's profile
+        if (!isProfilePrivate){
+            // adds "copy profile permalink" to the context menu
+            const copyPermalink = `<a class="popup_menu_item" href="#" id="copy_profile_perma_link"><img style="width: 16px; height: 16px" src="${chrome.runtime.getURL("images/paperclip.png")}">&nbsp; Copy Profile Permalink</a>`;
+            profileActionPopup.querySelector('.popup_body.popup_menu.shadow_content').insertAdjacentHTML('beforeend', copyPermalink);
 
-        // this is a workaround to only being able to copy text to the clipboard that is selected in a textbox
-        const textareaToCopy = `<textarea id="text_area_to_copy_permalink" style="position: absolute; left: -9999px" readonly="">https://steamcommunity.com/profiles/${profileOwnerSteamID}</textarea>`;
+            // this is a workaround to only being able to copy text to the clipboard that is selected in a textbox
+            const textareaToCopy = `<textarea id="text_area_to_copy_permalink" style="position: absolute; left: -9999px" readonly="">https://steamcommunity.com/profiles/${profileOwnerSteamID}</textarea>`;
 
-        document.getElementById('copy_profile_perma_link').addEventListener('click', () => {
-            document.querySelector('body').insertAdjacentHTML('beforeend', textareaToCopy);
-            let textAreaElement = document.getElementById('text_area_to_copy_permalink');
-            textAreaElement.select();
-            document.execCommand('copy');
-            textAreaElement.parentNode.removeChild(textAreaElement);
-        });
+            document.getElementById('copy_profile_perma_link').addEventListener('click', () => {
+                document.querySelector('body').insertAdjacentHTML('beforeend', textareaToCopy);
+                let textAreaElement = document.getElementById('text_area_to_copy_permalink');
+                textAreaElement.select();
+                document.execCommand('copy');
+                textAreaElement.parentNode.removeChild(textAreaElement);
+            });
 
-        // handles rep button related stuff
-        chrome.storage.local.get(['reputationMessage', 'showPlusRepButton'], (result) => {
-            if(result.showPlusRepButton){
-                let repButton = `<div style="float: right; text-align: center; margin-top: 6px;" class="commentthread_user_avatar playerAvatar"><span class="btn_green_white_innerfade btn_small" id="repper" style="padding: 5px;">+rep<span></div>`;
+            // handles rep button related stuff
+            chrome.storage.local.get(['reputationMessage', 'showPlusRepButton'], (result) => {
+                if(result.showPlusRepButton){
+                    let repButton = `<div style="float: right; text-align: center; margin-top: 6px;" class="commentthread_user_avatar playerAvatar"><span class="btn_green_white_innerfade btn_small" id="repper" style="padding: 5px;">+rep<span></div>`;
 
-                if (commentThreadEntryBox !== null) commentThreadEntryBox.insertAdjacentHTML('afterend', repButton);
+                    if (commentThreadEntryBox !== null) commentThreadEntryBox.insertAdjacentHTML('afterend', repButton);
 
-                document.getElementById('repper').addEventListener('click', () => {
-                    document.querySelector('.commentthread_textarea').value = result.reputationMessage;
-                    setTimeout(() => {document.querySelectorAll('.btn_green_white_innerfade.btn_small')[1].click()}, 500);
+                    document.getElementById('repper').addEventListener('click', () => {
+                        document.querySelector('.commentthread_textarea').value = result.reputationMessage;
+                        setTimeout(() => {document.querySelectorAll('.btn_green_white_innerfade.btn_small')[1].click()}, 500);
 
-                });
-            }
-            reportComments();
-        });
+                    });
+                }
+                reportComments();
+            });
+        }
     }
 
     chrome.storage.local.get('nsfwFilter', (result) => {
@@ -92,7 +98,7 @@ if (document.querySelector('body').classList.contains('profile_page')){
 
     // shows actual steam chat status if set in options
     chrome.storage.local.get('showRealStatus', (result) => {
-        if(result.showRealStatus){
+        if(result.showRealStatus && !isProfilePrivate){
             let statusDiv = document.querySelector('.profile_in_game.persona');
 
             if (statusDiv.classList.contains('online')){
