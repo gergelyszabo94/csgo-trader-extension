8eee97ba1e368405a442576b7d3010c1702b7b80
Gergely Szabo <geeryszaabo@gmail.com>
2019-08-31 16:13:22 +0200

    refactored to new code style

--- extension/chrome/js/background_scripts/messaging.js
+++ extension/chrome/js/background_scripts/messaging.js
@@ -1,26 +1,33 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.inventory !==undefined) {
-            chrome.storage.local.get(['itemPricing', 'prices', 'currency', 'exchangeRate', 'pricingProvider'], function(result) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        if (request.inventory !== undefined) {
+            chrome.storage.local.get(['itemPricing', 'prices', 'currency', 'exchangeRate', 'pricingProvider'], (result) => {
                 let prices = result.prices;
                 let steamID = request.inventory;
-                let xhr = new XMLHttpRequest();
-                xhr.open("GET", 'https://steamcommunity.com/profiles/' + steamID + '/inventory/json/730/2/?l=english', true);
-                xhr.onload = function (e) {
-                    let body = JSON.parse(xhr.responseText);
+
+                let getRequest = new Request(`https://steamcommunity.com/profiles/${steamID}/inventory/json/730/2/?l=english`);
+
+                fetch(getRequest).then((response) => {
+                    if (!response.ok) {
+                        sendResponse('error');
+                        console.log(response.ok);
+                    }
+                    else return response.json();
+                }).then((body) => {
                     let items = body.rgDescriptions;
                     let ids = body.rgInventory;
 
                     let itemsPropertiesToReturn = [];
                     let duplicates = {};
 
+
+                    // counts duplicates
                     for (let asset in ids) {
                         let assetid = ids[asset].id;
 
                         for (let item in items) {
                             if (ids[asset].classid === items[item].classid && ids[asset].instanceid === items[item].instanceid) {
                                 let market_hash_name = items[item].market_hash_name;
-                                if(duplicates[market_hash_name]===undefined){
+                                if (duplicates[market_hash_name] === undefined){
                                     let instances = [assetid];
                                     duplicates[market_hash_name] =
                                         {
@@ -29,7 +36,7 @@ chrome.runtime.onMessage.addListener(
                                         }
                                 }
                                 else{
-                                    duplicates[market_hash_name].num=duplicates[market_hash_name].num+1;
+                                    duplicates[market_hash_name].num = duplicates[market_hash_name].num+1;
                                     duplicates[market_hash_name].instances.push(assetid);
                                 }
                             }
@@ -59,7 +66,7 @@ chrome.runtime.onMessage.addListener(
                                 let quality = getQuality(items[item].type);
                                 let stickers =  parseStickerInfo(items[item].descriptions, 'direct');
                                 let nametag = undefined;
-                                let inspectLink ="";
+                                let inspectLink = '';
                                 let owner = steamID;
                                 let price = null;
 
@@ -115,24 +122,15 @@ chrome.runtime.onMessage.addListener(
                             }
                         }
                     }
-
-                    itemsPropertiesToReturn.sort((a, b) => { return a.position - b.position});
-                    sendResponse({inventory: itemsPropertiesToReturn});
-                };
-                try {
-                    xhr.send();
-                }
-                catch (e) {
-                    console.log(e);
-                    sendResponse("error");
-                }
+                    sendResponse({inventory: itemsPropertiesToReturn.sort((a, b) => { return a.position - b.position})});
+                });
             });
-            return true; //async return to signal that it will return later
+            return true; // async return to signal that it will return later
         }
-        else if (request.inventoryTotal!==undefined){
+        else if (request.inventoryTotal !== undefined){
             let inventory = request.inventoryTotal;
             let total = 0.0;
-            chrome.storage.local.get(['prices', 'exchangeRate', 'currency', 'pricingProvider'], function(result){
+            chrome.storage.local.get(['prices', 'exchangeRate', 'currency', 'pricingProvider'], (result) => {
                 inventory.forEach(item => {
                     total += parseFloat(getPrice(item.market_hash_name, item.dopplerInfo, result.prices, result.pricingProvider, result.exchangeRate, result.currency).price);
                 });
@@ -140,146 +138,118 @@ chrome.runtime.onMessage.addListener(
             });
             return true;
         }
-        else if (request.addPricesToInventory!==undefined){
+        else if (request.addPricesToInventory !== undefined){
             let inventory = request.addPricesToInventory;
-            chrome.storage.local.get(['prices', 'exchangeRate', 'currency', 'itemPricing', 'pricingProvider'], function(result){
+            chrome.storage.local.get(['prices', 'exchangeRate', 'currency', 'itemPricing', 'pricingProvider'], (result) =>{
                 if(result.itemPricing){
                     inventory.forEach(item =>{
-                        if(result.prices[item.market_hash_name] !== undefined && result.prices[item.market_hash_name] !== "null"){
+                        if(result.prices[item.market_hash_name] !== undefined && result.prices[item.market_hash_name] !== 'null'){
                             item.price =  getPrice(item.market_hash_name, item.dopplerInfo, result.prices, result.pricingProvider, result.exchangeRate, result.currency);
                         }
                     });
                     sendResponse({addPricesToInventory: inventory});
                 }
-                else{
-                    sendResponse({addPricesToInventory: inventory});
-                }
+                else sendResponse({addPricesToInventory: inventory});
             });
             return true;
         }
-        else if (request.badgetext!==undefined){
+        else if (request.badgetext !== undefined){
             chrome.browserAction.setBadgeText({text: request.badgetext});
             sendResponse({badgetext: request.badgetext})
         }
-        else if (request.openInternalPage!==undefined){
-            chrome.permissions.contains({
-                permissions: ['tabs']
-            }, function(result) {
+        else if (request.openInternalPage !== undefined){
+            chrome.permissions.contains({permissions: ['tabs']}, (result) => {
                 if (result) {
                     goToInternalPage(request.openInternalPage);
                     sendResponse({openInternalPage: request.openInternalPage});
                 }
-                else{
-                    sendResponse({openInternalPage: "no_tabs_api_access"});
-                }
+                else sendResponse({openInternalPage: 'no_tabs_api_access'});
             });
             return true;
         }
-        else if (request.setAlarm!==undefined){
+        else if (request.setAlarm !== undefined){
             chrome.alarms.create(request.setAlarm.name, {when: new Date(request.setAlarm.when).valueOf()});
-            // chrome.alarms.getAll(function(alarms){
-            //     console.log(alarms);
-            // });
+            // chrome.alarms.getAll((alarms) => {console.log(alarms)});
             sendResponse({setAlarm: request.setAlarm})
         }
-        else if (request.apikeytovalidate !==undefined) {
-            let apiKey = request.apikeytovalidate;
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + apiKey + '&steamids=76561198036030455', true);
-            xhr.onload = function (e) {
+        else if (request.apikeytovalidate !== undefined) {
+            let getRequest = new Request(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${request.apikeytovalidate}&steamids=76561198036030455`);
+
+            fetch(getRequest).then((response) => {
+                if (!response.ok) {
+                    console.log(response.ok);
+                    sendResponse('error');
+                }
+                else return response.json();
+            }).then((body) => {
                 try {
-                    let body = JSON.parse(xhr.responseText);
-                    if(body.response.players[0].steamid==="76561198036030455"){
-                        sendResponse({valid: true});
-                    }
-                    else{
-                        sendResponse({valid: false});
-                    }
+                    if (body.response.players[0].steamid === '76561198036030455') sendResponse({valid: true});
+                    else sendResponse({valid: false});
                 }
                 catch (e) {
                     console.log(e);
                     sendResponse({valid: false});
                 }
-            };
-            try {
-                xhr.send();
-            }
-            catch (e) {
-                console.log(e);
-                sendResponse("error");
-            }
-            return true; //async return to signal that it will return later
+            });
+            return true; // async return to signal that it will return later
         }
-        else if (request.GetPlayerSummaries !==undefined) {
-            chrome.storage.local.get(['apiKeyValid', 'steamAPIKey'], function(result) {
+        else if (request.GetPlayerSummaries !== undefined) {
+            chrome.storage.local.get(['apiKeyValid', 'steamAPIKey'], (result) => {
                 if(result.apiKeyValid){
                     let apiKey = result.steamAPIKey;
-                    let steamid = request.GetPlayerSummaries;
-                    let xhr = new XMLHttpRequest();
-                    xhr.open("GET", 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + apiKey + '&steamids=' + steamid, true);
-                    xhr.onload = function (e) {
-                        try {
-                            let body = JSON.parse(xhr.responseText);
-                            sendResponse({personastate: body.response.players[0].personastate, apiKeyValid: true});
+                    let steamID = request.GetPlayerSummaries;
+
+                    let getRequest = new Request(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamID}`);
+
+                    fetch(getRequest).then((response) => {
+                        if (!response.ok){
+                            console.log(response.ok);
+                            sendResponse('error');
                         }
+                        else return response.json();
+                    }).then((body) => {
+                        try {sendResponse({personastate: body.response.players[0].personastate, apiKeyValid: true})}
                         catch (e) {
                             console.log(e);
+                            sendResponse('error');
                         }
-                    };
-                    try {
-                        xhr.send();
-                    }
-                    catch (e) {
-                        console.log(e);
-                    }
+                    });
                 }
-                else{
-                    sendResponse({apiKeyValid: false});
-                }
+                else sendResponse({apiKeyValid: false});
             });
-            return true; //async return to signal that it will return later
+            return true; // async return to signal that it will return later
         }
-        else if (request.getFloatInfo !==undefined) {
+        else if (request.getFloatInfo !== undefined) {
             let inspectLink = request.getFloatInfo;
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", 'https://api.csgofloat.com/?url=' + inspectLink, true);
-            xhr.onload = function (e) {
-                try {
-                    let body = JSON.parse(xhr.responseText);
-                    sendResponse({floatInfo: body.iteminfo});
+
+            let getRequest = new Request(`https://api.csgofloat.com/?url=${inspectLink}`);
+
+            fetch(getRequest).then((response) => {
+                if (!response.ok){
+                    console.log(response.ok);
+                    sendResponse('error');
                 }
-                catch (e) {
-                    console.log(e);
-                }
-            };
-            try {
-                xhr.send();
-            }
-            catch (e) {
-                console.log(e);
-            }
-            return true; //async return to signal that it will return later
+                else return response.json();
+            }).then((body) => {
+                sendResponse({floatInfo: body.iteminfo});
+            });
+            return true; // async return to signal that it will return later
         }
         else if (request.getSteamRepInfo !==undefined) {
             let steamID = request.getSteamRepInfo;
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", 'https://steamrep.com/api/beta4/reputation/' + steamID + "?json=1", true);
-            xhr.onload = function (e) {
-                try {
-                    let body = JSON.parse(xhr.responseText);
-                    sendResponse({SteamRepInfo: body.steamrep});
+
+            let getRequest = new Request(`https://steamrep.com/api/beta4/reputation/${steamID}?json=1`);
+
+            fetch(getRequest).then((response) => {
+                if (!response.ok){
+                    console.log(response.ok);
+                    sendResponse('error');
                 }
-                catch (e) {
-                    console.log(e);
-                }
-            };
-            try {
-                xhr.send();
-            }
-            catch (e) {
-                console.log(e);
-            }
-            return true; //async return to signal that it will return later
+                else return response.json();
+            }).then((body) => {
+                sendResponse({SteamRepInfo: body.steamrep});
+            });
+            return true; // async return to signal that it will return later
         }
     });
 
