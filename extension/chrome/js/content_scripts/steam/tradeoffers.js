4a375e2e00df980c92b61f0ed465a022275ef91a
Gergely Szabo <geeryszaabo@gmail.com>
2019-11-04 17:10:37 +0100

    fixes item with no price warning getting misplaced #77

--- extension/chrome/js/content_scripts/steam/tradeoffers.js
+++ extension/chrome/js/content_scripts/steam/tradeoffers.js
@@ -137,10 +137,10 @@ function addTotals(offers, items){
             let offerElement = document.getElementById(`tradeofferid_${offer.tradeofferid}`);
             let primaryHeader = offerElement.querySelector('.tradeoffer_items.primary').querySelector('.tradeoffer_items_header');
             primaryHeader.innerText += ` ${prettyPrintPrice(result.currency, (theirItemsTotal).toFixed(2))}`;
-            yourIncludesItemWIthNoPrice ? primaryHeader.innerText += ' - includes items with no price' : null;
+            theirIncludesItemWIthNoPrice ? primaryHeader.innerText += ' - includes items with no price' : null;
             let secondaryHeader = offerElement.querySelector('.tradeoffer_items.secondary').querySelector('.tradeoffer_items_header');
             secondaryHeader.innerText += ` ${prettyPrintPrice(result.currency, (yourItemsTotal).toFixed(2))}`;
-            theirIncludesItemWIthNoPrice ? secondaryHeader.innerText += ' - includes items with no price' : null;
+            yourIncludesItemWIthNoPrice ? secondaryHeader.innerText += ' - includes items with no price' : null;
 
             let profitOrLoss = theirItemsTotal - yourItemsTotal;
             offerElement.querySelector('.tradeoffer_header').insertAdjacentHTML('beforeend', `<span class="profitOrLoss" data-profit-or-loss="${profitOrLoss}">${prettyPrintPrice(result.currency, (profitOrLoss).toFixed(2))}</span>`);
