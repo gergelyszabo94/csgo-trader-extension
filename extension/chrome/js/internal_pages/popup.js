d269c76c7dbc3be212377edaca83761277f5d1ad
Gergely Szabo <geeryszaabo@gmail.com>
2019-11-09 19:46:48 +0100

    basic currency converter #38

--- extension/chrome/js/internal_pages/popup.js
+++ extension/chrome/js/internal_pages/popup.js
@@ -1,3 +1,16 @@
+function doCurrencyConversion(){
+    let number = parseInt(event.target.value);
+    let targetID = event.target.id;
+    let currencyLeft = currenciesLeft.options[currenciesLeft.selectedIndex].value;
+    let currencyRight = currenciesRight.options[currenciesRight.selectedIndex].value;
+    chrome.storage.local.get('exchangeRates', (result) => {
+        let exchangeRateLeft = parseFloat(result.exchangeRates[currencyLeft]);
+        let exchangeRateRight = parseFloat(result.exchangeRates[currencyRight]);
+        if (targetID === 'numberInputLeft') numberInputRight.value = (number / exchangeRateLeft) * exchangeRateRight;
+        else if (targetID === 'numberInputRight')  numberInputLeft.value = (number / exchangeRateRight) * exchangeRateLeft;
+    });
+}
+
 trackEvent({
     type: 'pageview',
     action: 'ExtensionPopupView'
@@ -27,3 +40,48 @@ chrome.storage.local.get(['popupLinks', 'steamIDOfUser'], (result) => {
         }
     });
 });
+
+
+let currenciesRight = document.createElement('select');
+currenciesRight.classList.add('select-theme');
+
+for (let currency of Object.keys(currencies)){
+    let option = document.createElement('option');
+    option.value = currencies[currency].short;
+    option.text = currencies[currency].short;
+    currenciesRight.add(option);
+}
+
+let currenciesLeft = currenciesRight.cloneNode(true);
+currenciesRight.id = 'currenciesRight';
+currenciesLeft.id = 'currenciesLeft';
+let calculator = document.getElementById('calculator');
+calculator.style.fontSize = '0.7rem';
+calculator.style.marginTop = '10px';
+let spanBetween = document.createElement('span');
+spanBetween.style.margin = '5px';
+spanBetween.innerText = ' to ';
+
+calculator.appendChild(currenciesLeft);
+calculator.appendChild(spanBetween);
+calculator.appendChild(currenciesRight);
+
+let numberInputRight = document.createElement('input');
+numberInputRight.type = 'number';
+numberInputRight.classList.add('numberPicker');
+numberInputRight.style.width = '5rem';
+let numberInputLeft = numberInputRight.cloneNode(true);
+numberInputLeft.id = 'numberInputLeft';
+numberInputRight.id = 'numberInputRight';
+calculator.appendChild(numberInputLeft);
+let betweenNumbers = document.createElement('span');
+betweenNumbers.innerText = ' - ';
+calculator.appendChild(betweenNumbers);
+calculator.appendChild(numberInputRight);
+
+chrome.storage.local.get('currency', (result) => {
+    currenciesLeft.querySelector(`[value='${result.currency}']`).selected = true;
+});
+
+numberInputLeft.addEventListener('input', doCurrencyConversion);
+numberInputRight.addEventListener('input', doCurrencyConversion);
