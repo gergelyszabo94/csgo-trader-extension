90bb78d647bdf0525cc2fd45a9b85fca5bc569bf
Gergely Szabo <geeryszaabo@gmail.com>
2019-11-14 00:17:22 +0100

    previously missing items added dynamically, fixes #86

--- backend/priceScraper/priceScraper.py
+++ backend/priceScraper/priceScraper.py
@@ -20,13 +20,13 @@ def lambda_handler(event, context):
     try:
         response = requests.get("http://csgobackpack.net/api/GetItemsList/v2/")
     except Exception as e:
-            print(e)
-            error = "Error during csgobackpack request"
-            alert_via_sns(f'{error}: {e}')
-            return {
-                'statusCode': 500,
-                'body': error
-            }
+        print(e)
+        error = "Error during csgobackpack request"
+        alert_via_sns(f'{error}: {e}')
+        return {
+            'statusCode': 500,
+            'body': error
+        }
 
     print("Received response from csgobackpack.net")
 
@@ -64,13 +64,13 @@ def lambda_handler(event, context):
     try:
         response = requests.get("https://bitskins.com/api/v1/get_all_item_prices/?api_key=" + bitskins_api_key + "&code=" + bitskins_token.now() + "&app_id=730")
     except Exception as e:
-            print(e)
-            error = "Error during bitskins request"
-            alert_via_sns(f'{error}: {e}')
-            return {
-                'statusCode': 500,
-                'body': error
-            }
+        print(e)
+        error = "Error during bitskins request"
+        alert_via_sns(f'{error}: {e}')
+        return {
+            'statusCode': 500,
+            'body': error
+        }
 
     if response.status_code == 200 and response.json()['status'] == "success":
         print("Extracting pricing info")
@@ -81,155 +81,6 @@ def lambda_handler(event, context):
             price = item.get('price')
             instant_sale_price = item.get('instant_sale_price')
             skewness = item.get('skewness')
-            missingFromCsgobackpack = [
-                "P250 | Nevermore (Well-Worn)",
-                "Sticker | Astralis (Holo) | London 2018",
-                "Sticker | Astralis | London 2018",
-                "Sticker | Avangar (Holo) | Boston 2018",
-                "Sticker | Avangar | Boston 2018",
-                "Sticker | BIG (Foil) | London 2018",
-                "Sticker | BIG (Holo) | London 2018",
-                "Sticker | BIG | London 2018",
-                "Sticker | Blue Swallow",
-                "Sticker | Cheongsam",
-                "Sticker | Cheongsam (Holo)",
-                "Sticker | Cloud9 (Holo) | Boston 2018",
-                "Sticker | Cloud9 | Boston 2018",
-                "Sticker | FaZe Clan (Holo) | Boston 2018",
-                "Sticker | FaZe Clan | Boston 2018",
-                "Sticker | Fancy Koi",
-                "Sticker | Fancy Koi (Foil)",
-                "Sticker | Flash Gaming | Boston 2018",
-                "Sticker | Flipsid3 Tactics | Boston 2018",
-                "Sticker | G2 Esports (Foil) | Boston 2018",
-                "Sticker | G2 Esports | Boston 2018",
-                "Sticker | Gambit Esports (Holo) | London 2018",
-                "Sticker | Gambit Esports | London 2018",
-                "Sticker | God of Fortune",
-                "Sticker | Green Swallow",
-                "Sticker | Guardian Dragon",
-                "Sticker | Guardian Dragon (Foil)",
-                "Sticker | HellRaisers (Holo) | London 2018",
-                "Sticker | HellRaisers | London 2018",
-                "Sticker | Hotpot",
-                "Sticker | Huaji",
-                "Sticker | JW | Katowice 2019",
-                "Sticker | Longevity",
-                "Sticker | Longevity (Foil)",
-                "Sticker | Mahjong Fa",
-                "Sticker | Mahjong Rooster",
-                "Sticker | Mahjong Zhong",
-                "Sticker | Misfits Gaming (Foil) | Boston 2018",
-                "Sticker | Misfits Gaming | Boston 2018",
-                "Sticker | Natus Vincere | Boston 2018",
-                "Sticker | Natus Vincere (Holo) | Boston 2018",
-                "Sticker | Nezha",
-                "Sticker | Ninjas in Pyjamas (Foil) | London 2018",
-                "Sticker | Ninjas in Pyjamas (Holo) | London 2018",
-                "Sticker | Ninjas in Pyjamas | London 2018",
-                "Sticker | Ninjas in Pyjamas | Katowice 2015",
-                "Sticker | Non-Veg",
-                "Sticker | Noodles",
-                "Sticker | North (Foil) | London 2018",
-                "Sticker | North (Holo) | London 2018",
-                "Sticker | North | London 2018",
-                "Sticker | OpTic Gaming (Foil) | London 2018",
-                "Sticker | OpTic Gaming (Holo) | London 2018",
-                "Sticker | OpTic Gaming | London 2018",
-                "Sticker | PENTA Sports | Katowice 2015",
-                "Sticker | Phoenix",
-                "Sticker | Pixiu",
-                "Sticker | Pixiu (Foil)",
-                "Sticker | Quantum Bellator Fire | Boston 2018",
-                "Sticker | Rage",
-                "Sticker | Renegades (Foil) | Boston 2018",
-                "Sticker | Renegades | Boston 2018",
-                "Sticker | Renegades (Holo) | London 2018",
-                "Sticker | Renegades | London 2018",
-                "Sticker | Rice Bomb",
-                "Sticker | Rogue (Foil) | London 2018",
-                "Sticker | Rogue (Holo) | London 2018",
-                "Sticker | Rogue | London 2018",
-                "Sticker | Shaolin",
-                "Sticker | Space Soldiers (Foil) | Boston 2018",
-                "Sticker | Space Soldiers | Boston 2018",
-                "Sticker | Space Soldiers (Foil) | London 2018",
-                "Sticker | Space Soldiers (Holo) | London 2018",
-                "Sticker | Space Soldiers | London 2018",
-                "Sticker | Sprout Esports | Boston 2018",
-                "Sticker | Team EnVyUs (Holo) | Boston 2018",
-                "Sticker | Team EnVyUs | Boston 2018",
-                "Sticker | Team Liquid (Foil) | London 2018",
-                "Sticker | Team Liquid (Holo) | London 2018",
-                "Sticker | Team Liquid | Boston 2018",
-                "Sticker | Team Liquid | London 2018",
-                "Sticker | Team Spirit (Holo) | London 2018",
-                "Sticker | Team Spirit | London 2018",
-                "Sticker | Terror Rice",
-                "Sticker | Toy Tiger",
-                "Sticker | Twin Koi",
-                "Sticker | Twin Koi (Holo)",
-                "Sticker | Tyloo (Foil) | London 2018",
-                "Sticker | Tyloo (Holo) | London 2018",
-                "Sticker | Tyloo | London 2018",
-                "Sticker | Vega Squadron (Holo) | London 2018",
-                "Sticker | Vega Squadron | Boston 2018",
-                "Sticker | Vega Squadron | London 2018",
-                "Sticker | Virtus.Pro (Holo) | London 2018",
-                "Sticker | Virtus.Pro | London 2018",
-                "Sticker | Vox Eminor | Katowice 2015",
-                "Sticker | Water Gun",
-                "Sticker | Zombie Hop",
-                "Sticker | compLexity Gaming (Foil) | London 2018",
-                "Sticker | compLexity Gaming (Holo) | London 2018",
-                "Sticker | compLexity Gaming | London 2018",
-                "Sticker | device | Katowice 2019",
-                "Sticker | mousesports | Boston 2018",
-                "Sticker | niko (Foil) | London 2018",
-                "Sticker | niko | London 2018",
-                "★ Navaja Knife | Boreal Forest (Factory New)",
-                "★ Navaja Knife | Damascus Steel (Battle-Scarred)",
-                "★ Navaja Knife | Forest DDPAT (Factory New)",
-                "★ Navaja Knife | Night Stripe (Factory New)",
-                "★ Navaja Knife | Rust Coat (Well-Worn)",
-                "★ Navaja Knife | Scorched (Factory New)",
-                "★ StatTrak™ Bayonet | Forest DDPAT (Factory New)",
-                "★ StatTrak™ Bayonet | Night (Factory New)",
-                "★ StatTrak™ Bayonet | Scorched (Factory New)",
-                "★ StatTrak™ Huntsman Knife | Boreal Forest (Factory New)",
-                "★ StatTrak™ Karambit | Scorched (Factory New)",
-                "★ StatTrak™ Navaja Knife | Boreal Forest (Well-Worn)",
-                "★ StatTrak™ Navaja Knife | Night Stripe (Battle-Scarred)",
-                "★ StatTrak™ Navaja Knife | Night Stripe (Well-Worn)",
-                "★ StatTrak™ Navaja Knife | Rust Coat (Well-Worn)",
-                "★ StatTrak™ Navaja Knife | Scorched (Well-Worn)",
-                "★ StatTrak™ Navaja Knife | Tiger Tooth (Minimal Wear)",
-                "★ StatTrak™ Navaja Knife | Ultraviolet (Battle-Scarred)",
-                "★ StatTrak™ Shadow Daggers | Scorched (Factory New)",
-                "★ StatTrak™ Stiletto Knife | Damascus Steel (Well-Worn)",
-                "★ StatTrak™ Stiletto Knife | Forest DDPAT (Battle-Scarred)",
-                "★ StatTrak™ Stiletto Knife | Forest DDPAT (Well-Worn)",
-                "★ StatTrak™ Stiletto Knife | Rust Coat (Battle-Scarred)",
-                "★ StatTrak™ Stiletto Knife | Rust Coat (Well-Worn)",
-                "★ StatTrak™ Stiletto Knife | Stained (Factory New)",
-                "★ StatTrak™ Stiletto Knife | Ultraviolet (Minimal Wear)",
-                "★ StatTrak™ Stiletto Knife | Ultraviolet (Well-Worn)",
-                "★ StatTrak™ Talon Knife | Damascus Steel (Minimal Wear)",
-                "★ StatTrak™ Talon Knife | Fade (Minimal Wear)",
-                "★ StatTrak™ Talon Knife | Ultraviolet (Well-Worn)",
-                "★ StatTrak™ Talon Knife | Urban Masked (Well-Worn)",
-                "★ StatTrak™ Ursus Knife | Blue Steel (Minimal Wear)",
-                "★ StatTrak™ Ursus Knife | Damascus Steel (Minimal Wear)",
-                "★ StatTrak™ Ursus Knife | Forest DDPAT (Well-Worn)",
-                "★ StatTrak™ Ursus Knife | Ultraviolet (Field-Tested)",
-                "★ Stiletto Knife | Forest DDPAT (Factory New)",
-                "★ Stiletto Knife | Night Stripe (Factory New)",
-                "★ Stiletto Knife | Night Stripe (Field-Tested)",
-                "★ Talon Knife | Damascus Steel (Well-Worn)",
-                "★ Talon Knife | Rust Coat (Well-Worn)",
-                "★ Ursus Knife | Boreal Forest (Factory New)",
-                "★ Ursus Knife | Forest DDPAT (Factory New)"
-            ]
 
             if name == "M4A1-S | Icarus Fell (Field-Tested)":
                 continue
@@ -237,10 +88,6 @@ def lambda_handler(event, context):
                 name = name.replace("M4A4 | Emperor", 'M4A4 | The Emperor')
             elif name == "Music Kit | Damjan Mravunac, The Talos Principal":
                 name = "Music Kit | Damjan Mravunac, The Talos Principle"
-            elif name in missingFromCsgobackpack:
-                extract[name] = {
-                    "csgobackpack": "null"
-                }
             elif name == "Sticker | AdreN (Foil) | MLG Columbus 2016":
                 name = "Sticker | AdreN (Foil)  | MLG Columbus 2016"
             elif name == "Sticker | Boom(Foil)":
@@ -305,7 +152,16 @@ def lambda_handler(event, context):
                     "skewness": skewness
                 }
             except KeyError:
-                print(name)
+                print(name + " was not previously present, added")
+                extract[name] = {
+                    "csgobackpack": "null",
+                    "bitskins": {
+                        "pricing_mode": pricing_mode,
+                        "price": price,
+                        "instant_sale_price": instant_sale_price,
+                        "skewness": skewness
+                    }
+                }
         for item in extract:
             try:
                 extract[item]['bitskins'] = extract[item]['bitskins']
@@ -334,13 +190,13 @@ def lambda_handler(event, context):
     try:
         response = requests.get("https://loot.farm/fullprice.json")
     except Exception as e:
-            print(e)
-            error = "Error during loot.farm request"
-            alert_via_sns(f'{error}: {e}')
-            return {
-                'statusCode': 500,
-                'body': error
-            }
+        print(e)
+        error = "Error during loot.farm request"
+        alert_via_sns(f'{error}: {e}')
+        return {
+            'statusCode': 500,
+            'body': error
+        }
     print("Received response from loot.farm")
 
     if response.status_code == 200:
@@ -432,59 +288,8 @@ def lambda_handler(event, context):
             name = item.get('m').replace("/", '-')
             price = item.get('a')
 
-            missingFromCsgobackpack = [
-                "Souvenir AWP | Pit Viper (Battle-Scarred)",
-                "Souvenir Five-SeveN | Silver Quartz (Well-Worn)",
-                "Souvenir P2000 | Granite Marbleized (Factory New)",
-                "★ StatTrak™ Bayonet | Crimson Web (Factory New)",
-                "★ StatTrak™ Bowie Knife | Forest DDPAT (Factory New)",
-                "★ StatTrak™ Karambit | Forest DDPAT (Factory New)",
-                "★ StatTrak™ Karambit | Urban Masked (Factory New)",
-                "★ StatTrak™ M9 Bayonet | Boreal Forest (Factory New)",
-                "★ StatTrak™ M9 Bayonet | Urban Masked (Factory New)",
-                "★ StatTrak™ Ursus Knife | Scorched (Factory New)",
-                "★ StatTrak™ Ursus Knife | Forest DDPAT (Factory New)",
-                "★ Ursus Knife | Urban Masked (Factory New)",
-                "★ StatTrak™ Ursus Knife | Urban Masked (Factory New)",
-                "★ StatTrak™ Ursus Knife | Crimson Web (Battle-Scarred)",
-                "★ StatTrak™ Ursus Knife | Crimson Web (Factory New)",
-                "★ Ursus Knife | Crimson Web (Factory New)",
-                "★ StatTrak™ Ursus Knife | Boreal Forest (Factory New)",
-                "★ StatTrak™ Stiletto Knife | Case Hardened (Factory New)",
-                "★ Navaja Knife | Safari Mesh (Factory New)",
-                "★ StatTrak™ Stiletto Knife | Fade (Minimal Wear)",
-                "★ Talon Knife | Boreal Forest (Factory New)",
-                "★ Ursus Knife | Ultraviolet (Factory New)",
-                "★ Ursus Knife | Damascus Steel (Battle-Scarred)",
-                "★ StatTrak™ Ursus Knife | Ultraviolet (Battle-Scarred)",
-                "★ StatTrak™ Talon Knife | Ultraviolet (Factory New)",
-                "★ StatTrak™ Talon Knife | Damascus Steel (Well-Worn)",
-                "★ StatTrak™ Talon Knife | Damascus Steel (Battle-Scarred)",
-                "★ StatTrak™ Navaja Knife | Marble Fade (Minimal Wear)",
-                "★ StatTrak™ Ursus Knife | Damascus Steel (Battle-Scarred)",
-                "★ StatTrak™ Ursus Knife | Rust Coat (Well-Worn)",
-                "★ StatTrak™ Navaja Knife | Urban Masked (Factory New)",
-                "★ StatTrak™ Stiletto Knife | Tiger Tooth (Minimal Wear)",
-                "★ StatTrak™ Ursus Knife | Tiger Tooth (Minimal Wear)",
-                "★ Talon Knife | Urban Masked (Factory New)",
-                "★ StatTrak™ Ursus Knife | Marble Fade (Minimal Wear)",
-                "★ StatTrak™ Talon Knife | Tiger Tooth (Minimal Wear)",
-                "★ Talon Knife | Ultraviolet (Factory New)"
-            ]
-
             if "M4A4 | Emperor" in name:
                 name = name.replace("M4A4 | Emperor", 'M4A4 | The Emperor')
-            elif name in missingFromCsgobackpack:
-                extract[name] = {
-                    "csgobackpack": "null",
-                    "bitskins": "null",
-                    "lootfarm": "null",
-                    "csgotm": "null",
-                    # "csmoney": {
-                    #     'price': "null",
-                    #     'doppler': "null"
-                    # }
-                }
             elif name == "★ StatTrak™ Navaja Knife | Doppler Phase 1 (Minimal Wear)" or name == "★ StatTrak™ Navaja Knife | Doppler Phase 2 (Minimal Wear)" or name == "★ StatTrak™ Navaja Knife | Doppler Phase 3 (Minimal Wear)" or name == "★ StatTrak™ Navaja Knife | Doppler Phase 4 (Minimal Wear)" or name == "★ StatTrak™ Navaja Knife | Doppler Ruby (Minimal Wear)" or name == "★ StatTrak™ Navaja Knife | Doppler Sapphire (Minimal Wear)" or name == "★ StatTrak™ Navaja Knife | Doppler Black Pearl (Minimal Wear)":
                 try:
                     extract["★ StatTrak™ Navaja Knife | Doppler (Minimal Wear)"] = extract[
@@ -670,7 +475,17 @@ def lambda_handler(event, context):
                         'doppler': "null"
                     }
                 except KeyError:
-                    print(name)
+                    print(name + " was not previously present, added")
+                    extract[name] = {
+                        "csgobackpack": "null",
+                        "bitskins": "null",
+                        "lootfarm": "null",
+                        "csgotm": "null",
+                        "csmoney": {
+                            'price': price,
+                            'doppler': "null"
+                        }
+                    }
 
         for item in extract:
             try:
@@ -808,7 +623,7 @@ def get_csgobackpack_price(item, extract, daily_trend, weekly_trend):
     if "csgobackpack" in extract[item]:
         if "24_hours" in extract[item]["csgobackpack"] and "sold" in extract[item]["csgobackpack"]["24_hours"] and \
                 extract[item]["csgobackpack"]["24_hours"]["sold"] != "" and float(
-                extract[item]["csgobackpack"]["24_hours"]["sold"]) >= 5.0:
+            extract[item]["csgobackpack"]["24_hours"]["sold"]) >= 5.0:
             if abs(1 - float(extract[item]["csgobackpack"]["24_hours"]["average"]) / float(
                     extract[item]["csgobackpack"]["7_days"]["average"])) <= 0.1:
                 return extract[item]["csgobackpack"]["24_hours"]["average"]  # case A
@@ -820,8 +635,8 @@ def get_csgobackpack_price(item, extract, daily_trend, weekly_trend):
         elif "7_days" in extract[item]["csgobackpack"]:
             if float(extract[item]["csgobackpack"]["30_days"]["average"]) != 0.0 and abs(
                     1 - float(extract[item]["csgobackpack"]["7_days"]["average"]) / float(
-                            extract[item]["csgobackpack"]["30_days"]["average"])) <= 0.1 and float(
-                    extract[item]["csgobackpack"]["7_days"]["sold"]) >= 5.0:
+                        extract[item]["csgobackpack"]["30_days"]["average"])) <= 0.1 and float(
+                extract[item]["csgobackpack"]["7_days"]["sold"]) >= 5.0:
                 return float(extract[item]["csgobackpack"]["7_days"]["average"]) * daily_trend  # case D
             else:
                 return float(extract[item]["csgobackpack"]["30_days"]["average"]) * weekly_trend * daily_trend  # case E
