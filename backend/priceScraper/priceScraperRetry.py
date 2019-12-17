4b7ee57b56b03f73ad916c501eeabd39a5d51300
Gergely Szabo <geeryszaabo@gmail.com>
2019-12-18 00:21:37 +0100

    price scrapping retry resolves #107

---
+++ backend/priceScraper/priceScraperRetry.py
@@ -0,0 +1,28 @@
+import json
+import os
+import boto3
+from datetime import datetime
+
+bucket = os.environ['BUCKET']
+region = os.environ['REGION']
+object_key = os.environ['PRICES_OBJECT_KEY']
+
+s3 = boto3.client('s3', region_name=region)
+lbd = boto3.client('lambda', region_name=region)
+
+
+def lambda_handler(event, context):
+    result = s3.head_object(Bucket=bucket, Key=object_key)
+    last_modified = result['ResponseMetadata']['HTTPHeaders']['last-modified']
+    last_modified = datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S %Z')
+    seconds_since_last_update = (datetime.now() - last_modified).seconds
+
+    if seconds_since_last_update > 86400:  # if updated over a day ago
+        print('Prices have not been updated for over a day, calling priceScrapper')
+        response = lbd.invoke(
+            FunctionName='priceScraper',
+            InvocationType='Event',
+            Payload=json.dumps({}).encode('utf-8')
+        )
+    else:
+        print('No need to retry, let\'s see in an hour...')
