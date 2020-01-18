3c6fde3c3ce12660d828f8cb4b24bcc57f9d03c0
Gergely Szabo <geeryszaabo@gmail.com>
2020-01-18 14:58:52 +0100

    footer completed

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import React from 'react';
+import React, {Fragment}  from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import ReactGA from 'react-ga';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 import Navigation from './components/Navigation/Navigation';
 import Footer from './components/Footer/Footer';
@@ -17,53 +19,57 @@ import Faq from './containers/Faq/Faq';
 
 ReactGA.initialize('UA-48407333-4');
 
+library.add(fab);
+
 const App = () => {
     return (
         <Router>
-            <div>
+            <Fragment>
                 <Navigation/>
-                <Switch>
-                    <Route exact path="/">
-                        <Home
-                            gAnalytic={ReactGA}
-                        />
-                    </Route>
-                    <Route path="/changelog">
-                        <Changelog
-                            gAnalytic={ReactGA}
-                        />
-                    </Route>
-                    <Route path="/release-notes">
-                        <ReleaseNotes
-                            gAnalytic={ReactGA}
-                        />
-                    </Route>
-                    <Route path="/group">
-                        <SteamGroup
-                            gAnalytic={ReactGA}
-                        />
-                    </Route>
-                    <Route path="/prices">
-                        <Prices
-                            gAnalytic={ReactGA}
-                        />
-                    </Route>
-                    <Route path="/privacy">
-                        <Privacy
-                            gAnalytic={ReactGA}
-                        />
-                    </Route>
-                    <Route path="/faq">
-                        <Faq
-                            gAnalytic={ReactGA}
-                        />
-                    </Route>
-                    <Route>
-                        <Redirect to='/'/>
-                    </Route>
-                </Switch>
+                <div className='content'>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home
+                                gAnalytic={ReactGA}
+                            />
+                        </Route>
+                        <Route path='/changelog'>
+                            <Changelog
+                                gAnalytic={ReactGA}
+                            />
+                        </Route>
+                        <Route path='/release-notes'>
+                            <ReleaseNotes
+                                gAnalytic={ReactGA}
+                            />
+                        </Route>
+                        <Route path='/group'>
+                            <SteamGroup
+                                gAnalytic={ReactGA}
+                            />
+                        </Route>
+                        <Route path='/prices'>
+                            <Prices
+                                gAnalytic={ReactGA}
+                            />
+                        </Route>
+                        <Route path='/privacy'>
+                            <Privacy
+                                gAnalytic={ReactGA}
+                            />
+                        </Route>
+                        <Route path='/faq'>
+                            <Faq
+                                gAnalytic={ReactGA}
+                            />
+                        </Route>
+                        <Route>
+                            <Redirect to='/'/>
+                        </Route>
+                    </Switch>
+                </div>
                 <Footer/>
-            </div>
+            </Fragment>
         </Router>
     );
 };
--- site/src/components/Footer/Footer.js
+++ site/src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import {Link} from "react-router-dom";
 import NewTabLink from "..//NewTabLink/NewTabLink";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './Footer.css';
 
@@ -10,12 +11,28 @@ const footer = () => {
             <p>
                 A
                 <NewTabLink to='https://www.gergely-szabo.com/'> Gergely Szabo </NewTabLink>
-                project
+                project © {new Date().getFullYear()}
             </p>
-           <p>
-               © {new Date().getFullYear()}
-           </p>
+            <div>
+                <BrandIcon link='https://github.com/gergelyszabo94/csgo-trader-extension' brand='github'/>
+                <BrandIcon link='https://steamcommunity.com/groups/csgotraderextension' brand='steam'/>
+                <BrandIcon link='https://chrome.google.com/webstore/detail/csgo-trader-steam-trading/kaibcgikagnkfgjnibflebpldakfhfih' brand='chrome'/>
+                <BrandIcon link='https://addons.mozilla.org/en-US/firefox/addon/csgo-trader-steam-trading/' brand='firefox'/>
+            </div>
+            <span className='footerNavLink'>
+                <Link to='/privacy/'>Privacy</Link>
+            </span>
         </footer>
+    );
+};
+
+const BrandIcon = (props) => {
+    return (
+        <span className='brandIcon'>
+            <NewTabLink to={props.link}>
+                <FontAwesomeIcon icon={['fab', props.brand]} />
+            </NewTabLink>
+        </span>
     );
 };
 
