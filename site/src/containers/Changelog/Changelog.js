e1127ef9735d479a344e7c39909fd2dc42625fcf
Gergely Szabo <geeryszaabo@gmail.com>
2020-01-27 02:14:10 +0100

    changelog and release note for version 1.25

--- site/src/containers/Changelog/Changelog.js
+++ site/src/containers/Changelog/Changelog.js
@@ -18,6 +18,23 @@ const changelog = (props) => {
 
             <h1>Changelog</h1>
             <Container className='buildingBlock'>
+                <h2>Version 1.25 - 2020-01-27</h2>
+                <ul>
+                    <li>Added price highlighting for sell listings and buy orders, remove/cancel all or selected, as well as totals, history type highlighting and market history export on the Community Market page.
+                        Check out the <Link to='/release-notes/#1.25'> Release Notes </Link> for more info.
+                        Thanks to <NewTabLink to='https://steamcommunity.com/profiles/76561198085748819'> Wiesenmeister </NewTabLink> and
+                        <NewTabLink to='https://www.reddit.com/user/timgotpaper'> /u/timgotpaper </NewTabLink>
+                        for the ideas.
+                        <GithubIssueLink issueNumber={88}/>
+                    </li>
+                    <li>Added <Link to='/faq/'> Frequently Asked Questions (FAQ) page</Link></li>
+                    <li>Removed unused activeTab permission</li>
+                    <li>Updated the <Link to='/privacy/'> Privacy page </Link> with more detailed information</li>
+                    <li>NSFW mode now removes "holiday cheer" as well</li>
+                    <li>CSGOLounge bumping now works on old.csgolounge.com/mytrades as well</li>
+                </ul>
+            </Container>
+            <Container className='buildingBlock'>
                 <h2>Version 1.24 - 2020-01-09</h2>
                 <ul>
                     <li>Profiles with csgotrader.app in their name are highlighted with gold color Check out the
--- site/src/containers/ReleaseNotes/ReleaseNotes.js
+++ site/src/containers/ReleaseNotes/ReleaseNotes.js
@@ -19,6 +19,57 @@ const releaseNotes = (props) => {
             <Container className='buildingBlock'>
                 Release notes are meant to explain how to use new features and why certain design or policy decisions were made.
             </Container>
+            <Container className='buildingBlock' id='1.25'>
+                <h2>1.25 - New Community Market homepage features, price highlighting, market history export</h2>
+                <p>This update brought to some nice new features to the
+                    <NewTabLink to='https://steamcommunity.com/market/'> Steam Community Market </NewTabLink> homepage.
+                    Market lowest listing prices or "starting at" prices are loaded for every market listing and are highlighted
+                    in green if your listing has the same price as the lowest or in red if they do not.
+                    Mind you, your listing having the same price as the lowest listing does not always mean that your listing is the first one in the list on the market!
+                    An additional column with checkboxes was added to the listings table.
+                    You can select the listings you want removed then click the header to remove them.
+                    The total price of the listings on the current page is also added to the bottom.
+                    You can see these illustrated here:
+                    <img src='/img/release-notes/market_listings_features.png'
+                         title='Community Market Listings Features'
+                         alt='Community Market Listings Features'
+                         className='showcaseImage showcase'/>
+                </p>
+                <p>
+                    Similarly, totals, cancel all and cancel selected functionality was added to buy order section as well.
+                    The price loaded here is not the lowest listing though, but the highest buy order's price.
+                    The highlighting also works the other way around, if your buy order is the highest it is marked with green.
+                    Otherwise it's red.
+                    <img src='/img/release-notes/buy_order_features.png'
+                         title='Buy order features'
+                         alt='Buy order features'
+                         className='showcaseImage showcase'/>
+                </p>
+                <p>
+                    You might have noticed a new tab appearing on the market main page besides your listings and history tabs.
+                    An "Export Market History" tab was added. I described its purpose there but I will paste it here too:
+                    "Exporting your market history can be great if you want to analyse it in a spreadsheet for example.
+                    A history event is either one of these three actions: a purchase, a sale or a listing creation.
+                    The result is a .csv file that you can open in Microsoft Excel or use programmatically.
+                    It is in utf-8 charset, if you see weird characters in your Excel you should try
+                    <NewTabLink to='https://www.itg.ias.edu/content/how-import-csv-file-uses-utf-8-character-encoding-0'> importing it as such.</NewTabLink>
+                    ".
+                    The extension requests your market history in batches of 50 events and with a 5 second delay between each request.
+                    I am definitely not the average Joe with my over a hundred thousand events, gathering my whole market history would take close to 3 hours.
+                    This is what the feature looks like in action, not particularly exciting, but wait for the result!
+                    <img src='/img/release-notes/market_history_export_inprogress.png'
+                         title='Market History Export in-action'
+                         alt='Market History Export in-action'
+                         className='showcaseImage showcase'/>
+                </p>
+                <p>
+                    Here are a few rows of what the result looks like:
+                    <img src='/img/release-notes/market_history_export_result.png'
+                         title='Market History Export result'
+                         alt='Market History Export result'
+                         className='showcaseImage showcase'/>
+                </p>
+            </Container>
             <Container className='buildingBlock' id='1.24'>
                 <h2>1.24 - Highlighted profiles, trade offer Widescreen goodness</h2>
                 <p>Along with many bug fixes and other improvements this update brought two small features.</p>
