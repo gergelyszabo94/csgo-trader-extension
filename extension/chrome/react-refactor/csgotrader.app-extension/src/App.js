b49665e77a1dd47aa8f27306bb81c363b59de7de
unknown <gruczika@gmail.com>
2020-02-08 14:26:30 +0100

    node-sass injected

--- extension/chrome/react-refactor/csgotrader.app-extension/src/App.js
+++ extension/chrome/react-refactor/csgotrader.app-extension/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
