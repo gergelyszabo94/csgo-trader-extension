f4e9b57bdca6ac965a97e953fe3c77e0c3335a55
Gergely Szabo <geeryszaabo@gmail.com>
2020-02-10 01:44:16 +0100

    general schema completed

--- extension/chrome/react-refactor/csgotrader.app-extension/src/pages/Options/optionsSchemas/general.js
+++ extension/chrome/react-refactor/csgotrader.app-extension/src/pages/Options/optionsSchemas/general.js
@@ -1,5 +1,8 @@
-import React, {Fragment} from 'react';
-import NewTabLink from 'components/NewTabLink/NewTabLink'
+import React, { Fragment } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+
+import NewTabLink from 'components/NewTabLink/NewTabLink';
 
 const generalSchema = {
     title: 'General',
@@ -8,28 +11,34 @@ const generalSchema = {
             name: 'Auto-set Steam API key',
             storageKey: 'autoSetSteamAPIKey',
             inputType: 'flipSwitch',
-            description: 'Automatically generates and adds the Steam API key to the extension when visiting <a href="https://steamcommunity.com/dev/apikey" target="_blank">steamcommunity.com/dev/apikey</a>'
+            description:
+                <Fragment>
+                     Automatically generates and adds the Steam API key to the extension when visiting <NewTabLink to='https://steamcommunity.com/dev/apikey'>steamcommunity.com/dev/apikey</NewTabLink>
+                </Fragment>
         },
         {
             name: 'Steam API key',
             storageKey: 'steamAPIKey',
             inputType: 'modalTextBox',
-            description: <Fragment>
-                             Allows the extension to make API requests to the <a href="https://developer.valvesoftware.com/wiki/Steam_Web_API" target="_blank">Steam Web API</a>,
-                             functions that need an API key will be tagged with this icon: <i class="fas fa-code apiIcon"></i><br/> You can get an API key by filling out
-                             <a href="https://steamcommunity.com/dev/apikey" target="_blank"> this form</a> (you can put anything in as domain name).
-                         </Fragment>
+            description:
+                <Fragment>
+                    Allows the extension to make API requests to the <NewTabLink to='https://developer.valvesoftware.com/wiki/Steam_Web_API'>Steam Web API</NewTabLink>,
+                    functions that need an API key will be tagged with this icon: <FontAwesomeIcon icon={faCode} className='apiIcon'/> <br/>
+                    You can get an API key by filling out
+                    <NewTabLink to='https://steamcommunity.com/dev/apikey'> this form</NewTabLink> (you can put anything in as domain name).
+                </Fragment>
         },
         {
             name: 'Tabs API access',
             storageKey: 'steamAPIKey',
             inputType: 'flipSwitchSpecial',
-            description: <Fragment>
-                            Grants the extension access to the
-                            <a href="https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Working_with_the_Tabs_API" target="_blank"> browser.tabs API</a>.
-                            It - for example - allows the extension to open a tab and put it in focus, allows notifications to be clickable, opens the bookmarks page when an item is bookmarked, etc.
-                            I am using it in this mode so it's also less likely to be buggy as well.
-                         </Fragment>
+            description:
+                <Fragment>
+                    Grants the extension access to the
+                    <NewTabLink to='https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Working_with_the_Tabs_API'> browser.tabs API</NewTabLink>.
+                    It - for example - allows the extension to open a tab and put it in focus, allows notifications to be clickable, opens the bookmarks page when an item is bookmarked, etc.
+                    I am using it in this mode so it's also less likely to be buggy as well.
+                </Fragment>
         },
         {
             name: 'Flag scam comments',
@@ -53,10 +62,11 @@ const generalSchema = {
              name: 'Collect usage data',
              storageKey: 'telemetryOn',
              inputType: 'flipSwitch',
-             description: <Fragment>
-                            Sends anonymous usage data to the developers to help discover problems and better understand how the extension is used. Check the
-                            <a href="https://csgotrader.app/privacy" target="_blank" title="Privacy"> Privacy </a> page for more information on how your data in handled.
-                          </Fragment>
+             description:
+                <Fragment>
+                    Sends anonymous usage data to the developers to help discover problems and better understand how the extension is used. Check the
+                    <NewTabLink to='https://csgotrader.app/privacy'> Privacy </NewTabLink> page for more information on how your data in handled.
+                </Fragment>
         },
         {
              name: 'Hide other extensions',
@@ -69,8 +79,7 @@ const generalSchema = {
              storageKey: 'showStickerPrice',
              inputType: 'flipSwitch',
              description: 'When turned on it puts the total price of the stickers applied on a weapon under the exterior indicator in inventories, trade offers and no the incoming trade offers page.'
-        }
-        ]
+        }]
     }
 
 export default generalSchema;
