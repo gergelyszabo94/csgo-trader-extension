3c2b0d30487b6a3e840f13dbf3a3f90e928ddcf1
Gergely Szabo <geeryszaabo@gmail.com>
2020-02-17 02:39:09 +0100

    added pricing provider and mode descriptions

--- extension/chrome/react-refactor/csgotrader.app-extension/src/components/Options/Inputs/PricingProvider/PricingProvider.js
+++ extension/chrome/react-refactor/csgotrader.app-extension/src/components/Options/Inputs/PricingProvider/PricingProvider.js
@@ -1,13 +1,18 @@
 /* globals updatePrices */
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Select from "components/Options/Inputs/Select/Select";
 
+import './PricingProvider.css';
+
 const PricingProvider = props => {
+    const [aboutProvider, setAboutProvider] = useState(props.options.csgotrader.description);
+    const [aboutMode, setAboutMode] = useState(props.options.csgotrader.pricing_modes.csgotrader.description);
+
     const options = [];
 
     for (const provider in props.options) {
-        for (const mode in props.options[provider]['pricing_modes']) {
-            const textModePart = provider === mode ? '' : ` -  ${props.options[provider]['pricing_modes'][mode].long}`;
+        for (const mode in props.options[provider].pricing_modes) {
+            const textModePart = provider === mode ? '' : ` -  ${props.options[provider].pricing_modes[mode].long}`;
             options.push({
                 key: `${provider}.${mode}`,
                 text: `${props.options[provider].long} ${textModePart}`
@@ -16,10 +21,13 @@ const PricingProvider = props => {
     }
 
     const setStorage = thisValue => {
-        chrome.storage.local.set({
-            pricingProvider: thisValue.split('.')[0],
-            pricingMode: thisValue.split('.')[1]
-        }, () => {
+        const pricingProvider = thisValue.split('.')[0];
+        const pricingMode = thisValue.split('.')[1];
+
+        setAboutProvider(props.options[pricingProvider].description);
+        setAboutMode(props.options[pricingProvider].pricing_modes[pricingMode].description);
+
+        chrome.storage.local.set({pricingProvider, pricingMode}, () => {
             updatePrices();
         });
     };
@@ -27,7 +35,12 @@ const PricingProvider = props => {
     const getStorage = () => {
         return new Promise((resolve, reject) => {
             chrome.storage.local.get(['pricingProvider', 'pricingMode'], result => {
-                resolve(`${result.pricingProvider}.${result.pricingMode}`);
+                const { pricingProvider, pricingMode } = result;
+
+                setAboutProvider(props.options[pricingProvider].description);
+                setAboutMode(props.options[pricingProvider].pricing_modes[pricingMode].description);
+
+                resolve(`${pricingProvider}.${pricingMode}`);
             });
         });
     };
@@ -40,8 +53,11 @@ const PricingProvider = props => {
                 foreignUseEffect={getStorage}
                 options={options}
             />
+            <div className='about'>
+                <b>About the provider:</b> <span>{aboutProvider}</span>
+            </div>
             <div>
-                About:
+                <b>About the pricing mode:</b> <span>{aboutMode}</span>
             </div>
         </Fragment>
     );
