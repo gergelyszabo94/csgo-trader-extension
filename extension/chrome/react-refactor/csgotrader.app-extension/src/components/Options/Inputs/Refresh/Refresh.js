96ce9ec8532c4a5a7fc8c34b728f4bd364d2ec23
Gergely Szabo <geeryszaabo@gmail.com>
2020-02-20 00:59:36 +0100

    price refresh spinning

--- extension/chrome/react-refactor/csgotrader.app-extension/src/components/Options/Inputs/Refresh/Refresh.js
+++ extension/chrome/react-refactor/csgotrader.app-extension/src/components/Options/Inputs/Refresh/Refresh.js
@@ -3,21 +3,34 @@
 import React, { Fragment, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
+import './Refresh.css';
 
 const Refresh = () => {
-    const [state, setState] = useState('');
+    const [state, setState] = useState({
+        message: '',
+        spin: false
+    });
 
     const onClickHandler = () => {
         updatePrices();
+        setState({...state, spin: true});
+
         setTimeout(() => {
-            setState('Prices refreshed');
+            setState({
+                spin: false,
+                message: 'Prices refreshed'
+            });
         }, 2000);
     };
 
     return (
         <Fragment>
-            <FontAwesomeIcon icon={faSync} className='whiteIcon' onClick={onClickHandler}/>
-            <div>{state}</div>
+            <FontAwesomeIcon
+                icon={faSync}
+                className={`whiteIcon ${state.spin ? 'rotate' : null}`}
+                onClick={onClickHandler}
+            />
+            <div>{state.message}</div>
         </Fragment>
     );
 };
