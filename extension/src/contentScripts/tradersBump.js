32d4c91629d5c478332c8611c9452506ee6fbba5
Gergely Szabo <geeryszaabo@gmail.com>
2020-03-18 15:13:48 +0100

    traders error page skipped

--- extension/src/contentScripts/tradersBump.js
+++ extension/src/contentScripts/tradersBump.js
@@ -22,8 +22,13 @@ trackEvent({
   action: 'TradersTradesView',
 });
 
-chrome.storage.local.get('tradersBump', (result) => {
-  if (result.tradersBump) {
+chrome.storage.local.get('tradersBump', ({ tradersBump }) => {
+  if (tradersBump) {
+    // redirects form the "The page you were looking for doesn't exist." page
+    if (window.location.href.includes('cf_chl_jschl_tk')) {
+      window.location.href = 'https://csgotraders.net/mytrades';
+    }
+
     // ugly way to wait for the trades to load and become "bumpable"
     setTimeout(() => {
       bump();
