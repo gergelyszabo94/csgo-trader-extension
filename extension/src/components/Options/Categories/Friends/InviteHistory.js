832c5ab5835f1cd3fff6d2ce6ba6d314618ce27a
Gergely Szabo <geeryszaabo@gmail.com>
2020-04-01 23:26:56 +0200

    invite history table styling, date formatting

--- extension/src/components/Options/Categories/Friends/InviteHistory.js
+++ extension/src/components/Options/Categories/Friends/InviteHistory.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import NewTabLink from 'components/NewTabLink/NewTabLink';
-import { dateToISODisplay } from 'utils/dateTime';
+import { dateToISODisplay, prettyTimeAgo } from 'utils/dateTime';
 import HistoryEvent from './HistoryEvent';
 
 const InviteHistory = () => {
@@ -15,29 +15,33 @@ const InviteHistory = () => {
   return (
     <>
       <h3>Friend Request History</h3>
-      <table>
-        <tr>
-          <th>User</th>
-          <th>Event</th>
-          <th>Time</th>
-        </tr>
-        {historyEvents.map((event) => {
-          return (
-            <tr key={event.steamID + event.type + event.timestamp}>
-              <td>
-                <NewTabLink to={`https://steamcommunity.com/profiles/${event.steamID}`}>
-                  {event.username}
-                </NewTabLink>
-              </td>
-              <td>
-                <HistoryEvent eventType={event.type} />
-              </td>
-              <td>
-                {dateToISODisplay(event.timestamp / 1000)}
-              </td>
-            </tr>
-          );
-        })}
+      <table className="inviteHistoryTable">
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Event</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {historyEvents.map((event) => {
+            return (
+              <tr key={event.steamID + event.type + event.timestamp}>
+                <td>
+                  <NewTabLink to={`https://steamcommunity.com/profiles/${event.steamID}`}>
+                    {event.username}
+                  </NewTabLink>
+                </td>
+                <td>
+                  <HistoryEvent eventType={event.type} />
+                </td>
+                <td title={dateToISODisplay(event.timestamp / 1000)}>
+                  {prettyTimeAgo(event.timestamp / 1000)}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </>
   );
