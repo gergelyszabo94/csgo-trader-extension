9ecc31ae0be54a062f5fc670e36bc8281b4bf724
Gergely Szabo <geeryszaabo@gmail.com>
2020-04-08 00:28:53 +0200

    friend requests history show all option, descriptions

--- extension/src/components/Options/Categories/Friends/Condition.js
+++ extension/src/components/Options/Categories/Friends/Condition.js
@@ -3,10 +3,12 @@ import { conditions } from 'utils/static/friendRequests';
 
 const Condition = ({ type, value }) => {
   return (
-    <>
+    <span
+      title={conditions[type].description}
+    >
       {conditions[type].pretty}
       {value !== undefined ? ` ${value}` : ''}
-    </>
+    </span>
   );
 };
 
--- extension/src/components/Options/Categories/Friends/InviteRules.js
+++ extension/src/components/Options/Categories/Friends/InviteRules.js
@@ -16,18 +16,20 @@ const InviteRules = () => {
       <h5>Invite Rules</h5>
       <div className="mb-3 font-size--s">
         <span>
-        You can set your own rules for incoming friend requests to be evaluated by.
-        It&apos;s useful for example to automatically ignore scammers.
+          You can set your own rules for incoming friend requests to be evaluated by.
+          The rules are evaluated in order, the first matching rule&apos;s action is applied.
+          The feature only works if you are logged into Steam in this browser.
+          It&apos;s useful for example to automatically ignore scammers.
         </span>
         <ApiKeyIndicator />
       </div>
       <table className="inviteRules">
         <thead>
           <tr>
-            <th>Number</th>
-            <th>Condition</th>
-            <th>Action</th>
-            <th>State</th>
+            <th title="Rule number">Number</th>
+            <th title="Condition to match incoming invites">Condition</th>
+            <th title="What action should the extension take when the invite matches the condition?">Action</th>
+            <th title="Turn the individual rules on of off">State</th>
           </tr>
         </thead>
         <tbody>
