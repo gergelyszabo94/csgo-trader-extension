bbc1adb0441fbf3fc16d5c9f3f06e87a23b34add
Gergely Szabo <geeryszaabo@gmail.com>
2020-04-19 19:00:26 +0200

    autobumping logic

--- extension/src/contentScripts/steam/discussions.js
+++ extension/src/contentScripts/steam/discussions.js
@@ -10,6 +10,30 @@ trackEvent({
   action: 'DiscussionsView',
 });
 
+let autoBumpInterval;
+const doTheAutoBumping = () => {
+  document.querySelectorAll('.commentthread_comment').forEach((commentThread) => {
+    const commentTextElement = commentThread.querySelector('.commentthread_comment_text');
+    if (commentTextElement !== null && commentTextElement.innerText.includes('Bump')) {
+      const deleteButton = commentThread.querySelector('.forum_comment_action.delete');
+      if (deleteButton !== null) deleteButton.click();
+      setTimeout(() => {
+        const deleteModal = document.querySelector('.newmodal');
+        if (deleteModal !== null) {
+          deleteModal.querySelector('.btn_green_white_innerfade.btn_medium').click();
+        }
+      }, 1000);
+      // sometimes users are prompted to confirm deletion
+    }
+  });
+  const commentTextArea = document.querySelector('.forumtopic_reply_entry').querySelector('.forumtopic_reply_textarea');
+  commentTextArea.value = 'Bump';
+  commentTextArea.focus();
+  setTimeout(() => {
+    document.querySelector('.btn_green_white_innerfade.btn_small').click();
+  }, 1000);
+};
+
 const searchElement = document.getElementById('DiscussionSearchForm');
 if (searchElement !== null) {
   searchElement.insertAdjacentHTML(
@@ -29,21 +53,36 @@ if (searchElement !== null) {
   const autoBumpCheckBox = document.getElementById('autoBumpDiscussion');
 
   chrome.storage.local.get(['discussionsToAutoBump'], ({ discussionsToAutoBump }) => {
-    autoBumpCheckBox.checked = discussionsToAutoBump.includes(window.location.href);
+    if (discussionsToAutoBump.includes(window.location.href)) {
+      autoBumpCheckBox.checked = true;
+      doTheAutoBumping();
+
+      autoBumpInterval = setInterval(() => {
+        doTheAutoBumping();
+      }, (15000)); // 30 minutes
+    } else autoBumpCheckBox.checked = false;
   });
 
   autoBumpCheckBox.addEventListener('change', (event) => {
     chrome.storage.local.get(['discussionsToAutoBump'], ({ discussionsToAutoBump }) => {
       let newDiscussionsToAutoBump = [];
       if (event.target.checked) {
+        autoBumpInterval = setInterval(() => {
+          doTheAutoBumping();
+        }, (30 * 60 * 1000)); // 30 minutes
+
         if (discussionsToAutoBump.includes(window.location.href)) {
           newDiscussionsToAutoBump = discussionsToAutoBump;
         } else newDiscussionsToAutoBump = [...discussionsToAutoBump, window.location.href];
-      } else if (discussionsToAutoBump.includes(window.location.href)) {
-        newDiscussionsToAutoBump = discussionsToAutoBump.filter((href) => {
-          return href !== window.location.href;
-        });
-      } else newDiscussionsToAutoBump = discussionsToAutoBump;
+      } else {
+        clearInterval(autoBumpInterval);
+
+        if (discussionsToAutoBump.includes(window.location.href)) {
+          newDiscussionsToAutoBump = discussionsToAutoBump.filter((href) => {
+            return href !== window.location.href;
+          });
+        } else newDiscussionsToAutoBump = discussionsToAutoBump;
+      }
 
       chrome.storage.local.set({ discussionsToAutoBump: newDiscussionsToAutoBump });
     });
