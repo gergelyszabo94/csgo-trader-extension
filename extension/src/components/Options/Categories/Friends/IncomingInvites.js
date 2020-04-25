8f83d02c3e5b84c99941ccc0302246e5b0bf0e7e
Gergely Szabo <geeryszaabo@gmail.com>
2020-04-25 17:21:53 +0200

    added number of friend requests and placeholder text when there aren't any #195

--- extension/src/components/Options/Categories/Friends/IncomingInvites.js
+++ extension/src/components/Options/Categories/Friends/IncomingInvites.js
@@ -79,74 +79,85 @@ const IncomingInvites = () => {
 
   return (
     <div className="my-5">
-      <h5>Incoming Friend Requests</h5>
-      <table className="spacedTable">
-        <thead>
-          <tr>
-            <th
-              title="Order by Steam username of requester at the time of the extension noticing the invite"
-              className="clickAbleColumnHeader"
-              onClick={() => {
-                if (sortingMode !== 'name_asc') sortInvites('name_asc');
-                else sortInvites('name_desc');
-              }}
-            >
-              Username
-            </th>
-            <th
-              title="Order by the user's Steam profile level"
-              className="clickAbleColumnHeader"
-              onClick={() => {
-                if (sortingMode !== 'level_desc') sortInvites('level_desc');
-                else sortInvites('level_asc');
-              }}
-            >
-              Steam Level
-            </th>
-            <th title="The country the user has set on their Steam profile">Country</th>
-            <th
-              title="Order by how many common friends you and this user has"
-              className="clickAbleColumnHeader"
-              onClick={() => {
-                if (sortingMode !== 'friends_desc') sortInvites('friends_desc');
-                else sortInvites('friends_asc');
-              }}
-            >
-              Common Friends
-            </th>
-            <th title="The user's Steam community profile's privacy state">Profile</th>
-            <th
-              title="Sort by the user's CS:GO inventory value"
-              className="clickAbleColumnHeader"
-              onClick={() => {
-                if (sortingMode !== 'invent_value_desc') sortInvites('invent_value_desc');
-                else sortInvites('invent_value_asc');
-              }}
-            >
-              Inventory Value
-            </th>
-            <th title="Your trade offer history with the user">Offers</th>
-            <th title="The summary fo the users's bans">Bans</th>
-            <th title="Accept, ignore or block the user's request">Actions</th>
-            <th title="The extension retries to gather all information for the friend request to be evaluated, this is the count of the retries">Evals</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            invites.map((invite, index) => {
-              return (
-                <Invite
-                  key={invite.steamID}
-                  details={invite}
-                  index={index}
-                  currency={curr}
-                  remove={removeInvite}
-                />
-              );
-            })
-          }
-        </tbody>
-      </table>
+      <h5>
+        Incoming Friend Requests
+        (
+        { invites.length }
+        )
+      </h5>
+      {
+        invites.length === 0
+          ? <div>You don&apos;t have any incoming requests at this time.</div>
+          : (
+            <table className="spacedTable">
+              <thead>
+                <tr>
+                  <th
+                    title="Order by Steam username of requester at the time of the extension noticing the invite"
+                    className="clickAbleColumnHeader"
+                    onClick={() => {
+                      if (sortingMode !== 'name_asc') sortInvites('name_asc');
+                      else sortInvites('name_desc');
+                    }}
+                  >
+                    Username
+                  </th>
+                  <th
+                    title="Order by the user's Steam profile level"
+                    className="clickAbleColumnHeader"
+                    onClick={() => {
+                      if (sortingMode !== 'level_desc') sortInvites('level_desc');
+                      else sortInvites('level_asc');
+                    }}
+                  >
+                    Steam Level
+                  </th>
+                  <th title="The country the user has set on their Steam profile">Country</th>
+                  <th
+                    title="Order by how many common friends you and this user has"
+                    className="clickAbleColumnHeader"
+                    onClick={() => {
+                      if (sortingMode !== 'friends_desc') sortInvites('friends_desc');
+                      else sortInvites('friends_asc');
+                    }}
+                  >
+                    Common Friends
+                  </th>
+                  <th title="The user's Steam community profile's privacy state">Profile</th>
+                  <th
+                    title="Sort by the user's CS:GO inventory value"
+                    className="clickAbleColumnHeader"
+                    onClick={() => {
+                      if (sortingMode !== 'invent_value_desc') sortInvites('invent_value_desc');
+                      else sortInvites('invent_value_asc');
+                    }}
+                  >
+                    Inventory Value
+                  </th>
+                  <th title="Your trade offer history with the user">Offers</th>
+                  <th title="The summary fo the users's bans">Bans</th>
+                  <th title="Accept, ignore or block the user's request">Actions</th>
+                  <th title="The extension retries to gather all information for the friend request to be evaluated, this is the count of the retries">Evals</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                invites.map((invite, index) => {
+                  return (
+                    <Invite
+                      key={invite.steamID}
+                      details={invite}
+                      index={index}
+                      currency={curr}
+                      remove={removeInvite}
+                    />
+                  );
+                })
+              }
+              </tbody>
+            </table>
+          )
+      }
     </div>
   );
 };
