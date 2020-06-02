edea337e2e1ec1cee0cada49d347c9f4a72a5921
unknown <gruczika@gmail.com>
2020-06-02 19:27:34 +0200

    Trade History separated, basic layout created

---
+++ extension/src/components/TradeOffer/TradeOffer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import NewTabLink from 'components/NewTabLink/NewTabLink';
+
+const TradeOffer = (props) => {
+  const urlIconToString = (iconURL) => {
+    return `https://steamcommunity.com/economy/image/${iconURL}/256x256`;
+  };
+
+  const itemUrlToString = (appid, marketHashName) => {
+    return `https://steamcommunity.com/market/listings/${appid}/${marketHashName}`;
+  };
+
+  const { title, assets } = props;
+
+  return assets !== undefined ? (
+    <div className="assets col-md-6">
+      <h3>{title}</h3>
+      <div className="assets__items">
+        {assets.map((asset) => {
+          return (
+            <div className="row assets__item ">
+              <div className="col-md-2">
+                <img
+                  className="assets__image"
+                  key={asset.assetid}
+                  src={urlIconToString(asset.icon_url)}
+                  alt={asset.market_hash_name}
+                />
+              </div>
+
+              <h3 className="col-md-8 assets__name">
+                <NewTabLink
+                  to={itemUrlToString(asset.appid, asset.market_hash_name)}
+                >
+                  {asset.market_hash_name}
+                </NewTabLink>
+              </h3>
+              <div className="col-md-2">
+                <span className="assets__price">price</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  ) : null;
+};
+
+export default TradeOffer;
--- extension/src/pages/TradeHistory/TradeHistory.js
+++ extension/src/pages/TradeHistory/TradeHistory.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { trackEvent } from 'utils/analytics';
 import { getTradeHistory } from 'utils/IEconService';
-import NewTabLink from 'components/NewTabLink/NewTabLink';
+import TradeOffer from 'components/TradeOffer/TradeOffer';
 
 const TradeHistory = () => {
   trackEvent({
@@ -24,79 +24,29 @@ const TradeHistory = () => {
       });
   }, []);
 
-  const urlIconToString = (iconURL) => {
-    return `https://steamcommunity.com/economy/image/${iconURL}/256x256`;
-  };
-
-  const itemUrlToString = (appid, marketHashName) => {
-    return `https://steamcommunity.com/market/listings/${appid}/${marketHashName}`;
-  };
-
   return (
-    <div>
+    <div className="container">
       <h1>Trade History</h1>
-      <div className="trade-histoy">
+      <div className="trade-histoy ">
         {trades !== undefined
           ? trades.map((trade) => {
             return (
-              <div className="trade-history__item" key={trade.tradeid}>
-                {trade.assets_given_desc.length !== 0 ? (
-                  <div className="assets">
-                    <h3>Assets given</h3>
-                    <div className="assets__items">
-                      {trade.assets_given_desc.map((asset) => {
-                        return (
-                          <div className="assets__item">
-                            <h3>
-                              <NewTabLink
-                                to={itemUrlToString(
-                                  asset.appid,
-                                  asset.market_hash_name,
-                                )}
-                              >
-                                {asset.market_hash_name}
-                              </NewTabLink>
-                            </h3>
-                            <img
-                              key={asset.assetid}
-                              src={urlIconToString(asset.icon_url)}
-                              alt={asset.market_hash_name}
-                            />
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </div>
-                ) : null}
-                {trade.assets_received_desc.length !== 0 ? (
-                  <div className="assets">
-                    <h3>Assets received</h3>
-
-                    <div className="assets__items">
-                      {trade.assets_received_desc.map((asset) => {
-                        return (
-                          <div className="assets__item">
-                            <h3>
-                              <NewTabLink
-                                to={itemUrlToString(
-                                  asset.appid,
-                                  asset.market_hash_name,
-                                )}
-                              >
-                                {asset.market_hash_name}
-                              </NewTabLink>
-                            </h3>
-                            <img
-                              key={asset.assetid}
-                              src={urlIconToString(asset.icon_url)}
-                              alt={asset.market_hash_name}
-                            />
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </div>
-                ) : null}
+              <div
+                className="row trade-history__list-item"
+                key={trade.tradeid}
+              >
+                {trade.assets_given_desc.length !== 0
+                  ? TradeOffer({
+                    title: 'Assets given',
+                    assets: trade.assets_given_desc,
+                  })
+                  : null}
+                {trade.assets_received_desc.length !== 0
+                  ? TradeOffer({
+                    title: 'Assets received',
+                    assets: trade.assets_received_desc,
+                  })
+                  : null}
               </div>
             );
           })
