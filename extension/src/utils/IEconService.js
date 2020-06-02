27be6c0b3ae4825743c36e8c0389d16a65d6ac58
Gergely Szabo <gergely@gergely-szabo.com>
2020-06-02 17:22:26 +0200

    added prices, totals and p/l to offers #104

--- extension/src/utils/IEconService.js
+++ extension/src/utils/IEconService.js
@@ -1,64 +1,96 @@
+import { getPrice } from 'utils/pricing';
+
 const getTradeHistory = (maxTrades, startTime = 0) => new Promise((resolve, reject) => {
-  chrome.storage.local.get(['apiKeyValid', 'steamAPIKey'], ({ apiKeyValid, steamAPIKey }) => {
-    if (apiKeyValid) {
-      const getRequest = new Request(
-        `https://api.steampowered.com/IEconService/GetTradeHistory/v1/?max_trades=${maxTrades}&start_after_time=${startTime}&get_descriptions=1&language=english&key=${steamAPIKey}`,
-      );
+  chrome.storage.local.get(['apiKeyValid', 'steamAPIKey', 'prices', 'provider', 'currency', 'exchangeRate'],
+    ({
+      apiKeyValid, steamAPIKey, prices, provider, currency, exchangeRate,
+    }) => {
+      if (apiKeyValid) {
+        const getRequest = new Request(
+          `https://api.steampowered.com/IEconService/GetTradeHistory/v1/?max_trades=${maxTrades}&start_after_time=${startTime}&get_descriptions=1&language=english&key=${steamAPIKey}`,
+        );
 
-      fetch(getRequest).then((response) => {
-        if (!response.ok) {
-          console.log(`Error code: ${response.status} Status: ${response.statusText}`);
-          reject(response.statusText);
-        } else return response.json();
-      }).then((body) => {
-        try {
-          if (body.response.trades) {
-            const trades = [];
-            body.response.trades.forEach((trade) => {
-              const tradeWithDesc = { ...trade };
-              tradeWithDesc.assets_received_desc = [];
-              tradeWithDesc.assets_given_desc = [];
-              if (trade.assets_received) {
-                trade.assets_received.forEach((received) => {
-                  const description = body.response.descriptions.filter((desc) => {
-                    return desc.appid === received.appid && desc.classid === received.classid
-                      && desc.instanceid === received.instanceid;
-                  });
-                  tradeWithDesc.assets_received_desc.push({
-                    ...received,
-                    ...description[0],
-                  });
-                });
-              }
+        fetch(getRequest).then((response) => {
+          if (!response.ok) {
+            console.log(`Error code: ${response.status} Status: ${response.statusText}`);
+            reject(response.statusText);
+          } else return response.json();
+        }).then((body) => {
+          try {
+            if (body.response.trades) {
+              const trades = [];
+              body.response.trades.forEach((trade) => {
+                const tradeWithDesc = {
+                  ...trade,
+                  receivedTotal: 0.0,
+                  givenTotal: 0.0,
+                };
+                tradeWithDesc.assets_received_desc = [];
+                tradeWithDesc.assets_given_desc = [];
 
-              if (trade.assets_given) {
-                trade.assets_given.forEach((given) => {
-                  const description = body.response.descriptions.filter((desc) => {
-                    return desc.appid === given.appid && desc.classid === given.classid
-                      && desc.instanceid === given.instanceid;
+                if (trade.assets_received) {
+                  trade.assets_received.forEach((received) => {
+                    const description = body.response.descriptions.filter((desc) => {
+                      return desc.appid === received.appid && desc.classid === received.classid
+                        && desc.instanceid === received.instanceid;
+                    });
+                    const price = getPrice(
+                      description[0].market_hash_name,
+                      null,
+                      prices,
+                      provider,
+                      exchangeRate,
+                      currency,
+                    );
+                    tradeWithDesc.receivedTotal += parseFloat(price.price);
+                    tradeWithDesc.assets_received_desc.push({
+                      ...received,
+                      ...description[0],
+                      price,
+                    });
                   });
-                  tradeWithDesc.assets_given_desc.push({
-                    ...given,
-                    ...description[0],
-                  });
-                });
-              }
+                }
 
-              trades.push(tradeWithDesc);
-            });
-            resolve(trades);
+                if (trade.assets_given) {
+                  trade.assets_given.forEach((given) => {
+                    const description = body.response.descriptions.filter((desc) => {
+                      return desc.appid === given.appid && desc.classid === given.classid
+                        && desc.instanceid === given.instanceid;
+                    });
+                    const price = getPrice(
+                      description[0].market_hash_name,
+                      null,
+                      prices,
+                      provider,
+                      exchangeRate,
+                      currency,
+                    );
+                    tradeWithDesc.givenTotal += parseFloat(price.price);
+                    tradeWithDesc.assets_given_desc.push({
+                      ...given,
+                      ...description[0],
+                      price,
+                    });
+                  });
+                }
+
+                tradeWithDesc.profitLoss = tradeWithDesc.receivedTotal - tradeWithDesc.givenTotal;
+
+                trades.push(tradeWithDesc);
+              });
+              resolve(trades);
+            }
+            reject('trades undefined');
+          } catch (e) {
+            console.log(e);
+            reject(e);
           }
-          reject('trades undefined');
-        } catch (e) {
-          console.log(e);
-          reject(e);
-        }
-      }).catch((err) => {
-        console.log(err);
-        reject(err);
-      });
-    } else reject('api_key_invalid');
-  });
+        }).catch((err) => {
+          console.log(err);
+          reject(err);
+        });
+      } else reject('api_key_invalid');
+    });
 });
 
 
