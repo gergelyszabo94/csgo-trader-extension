86f146330b2b6425dd268322ae75ae2b69462262
Gergely Szabo <gergely@gergely-szabo.com>
2020-06-08 20:49:39 +0200

    error message when the trade history doesn't load #104

--- extension/src/pages/TradeHistory/TradeHistory.js
+++ extension/src/pages/TradeHistory/TradeHistory.js
@@ -18,6 +18,7 @@ const TradeHistory = () => {
   const [historySize, setHistorySize] = useState(50);
   const [startTime, setStartTime] = useState(0);
   const [excludeEmpty, setExcludeEmpty] = useState(false);
+  const [error, setError] = useState(null);
 
   const updateTrades = () => {
     getTradeHistory(historySize, startTime)
@@ -32,6 +33,7 @@ const TradeHistory = () => {
       })
       .catch((err) => {
         console.log(err);
+        setError('Could not load your offer history, Steam might be done or you don\'t have your API key set.');
       });
   };
 
@@ -52,14 +54,20 @@ const TradeHistory = () => {
           setExcludeEmpty={setExcludeEmpty}
         />
 
-        {trades !== null ? (
-          <TradeOffers trades={trades} />
-        ) : (
-          <Spinner />
-        )}
+        {
+          error === null
+            ? <TradeOffersContent trades={trades} />
+            : <div className="warning">{error}</div>
+        }
       </div>
     </div>
   );
+};
+
+const TradeOffersContent = ({ trades }) => {
+  return trades !== null
+    ? (<TradeOffers trades={trades} />)
+    : (<Spinner />);
 };
 
 export default TradeHistory;
