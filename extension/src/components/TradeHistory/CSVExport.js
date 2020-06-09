4ef570e4f3a6fbe37e1b1787bf5aafb015c8832d
Gergely Szabo <gergely@gergely-szabo.com>
2020-06-09 18:21:11 +0200

    export now independent of loaded trades #104

--- extension/src/components/TradeHistory/CSVExport.js
+++ extension/src/components/TradeHistory/CSVExport.js
@@ -1,37 +1,61 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileExport, faPlay } from '@fortawesome/free-solid-svg-icons';
-import CustomA11yButton from 'components/CustomA11yButton/CustomA11yButton';
+import DatePicker from 'react-datepicker/es';
 
-const CSVExport = ({ trades }) => {
-  const [csvContent, setCsvContent] = useState(
-    'Partner SteamID,Partner Name,P/L,Given Total,Received Total,Items Given,Items Taken\n',
-  );
+import CustomA11yButton from 'components/CustomA11yButton/CustomA11yButton';
+import { getTradeHistory } from 'utils/IEconService';
+
+const CSVExport = () => {
+  let csvContent = 'Partner SteamID,Partner Name,Time,P/L,Given Total,Received Total,Items Given,Items Taken\n';
+
+  const [downloadURI, setDownloadURI] = useState('');
   const [showDownload, setShowDownload] = useState(false);
   const [statusMessage, setStatusMessage] = useState('');
   const [exclude, setExclude] = useState(false);
+  const [exportStartTime, setExportStartTime] = useState(Date.now());
+  const [exportEndTime, setExportEndTime] = useState(Date.now());
+  const [exportStartTimeUnix, setExportStartTimeUnix] = useState(
+    parseInt(new Date().getTime() / 1000),
+  );
+  const [exportEndTimeUnix, setExportEndTimeUnix] = useState(parseInt(new Date().getTime() / 1000));
+  const [requestCount, setRequestCount] = useState(0);
+
+  const finishExport = () => {
+    const encodedURI = `data:text/csv;charset=utf-8,${encodeURIComponent(csvContent)}`;
+    setDownloadURI(encodedURI);
+    setStatusMessage('Export finished, click the file icon to download the resulting .CSV file');
+    setShowDownload(true);
+  };
+
+  const loadNextChunk = (startTime, lastTradeID) => {
+    setRequestCount(requestCount + 1);
+    setStatusMessage(`Request ${requestCount} received`);
+    getTradeHistory(100, startTime, lastTradeID).then((tradesResponse) => {
+      let lines = '';
+      tradesResponse.trades.forEach((trade) => {
+        if ((!exclude || (exclude
+          && (trade.assets_given_desc !== 0 && trade.assets_received_desc.length !== 0)))
+          && exportEndTimeUnix >= trade.time_init) {
+          let givenItems = '';
+          trade.assets_given_desc.forEach((item) => {
+            givenItems += `${item.market_hash_name}(${item.price.price}),`;
+          });
+          let receivedItems = '';
+          trade.assets_received_desc.forEach((item) => {
+            receivedItems += `${item.market_hash_name}(${item.price.price}),`;
+          });
+          lines += `${trade.steamid_other},${trade.partnerSummary.personaname},${new Date(trade.time_init * 1000)},${trade.profitLoss},${trade.givenTotal},${trade.receivedTotal},"${givenItems}","${receivedItems}"\n`;
+        }
+      });
+      csvContent += lines;
+      finishExport();
+    });
+  };
 
   const startExport = () => {
     setStatusMessage('Exporting in progress');
-    let lines = '';
-    trades.forEach((trade) => {
-      if (!exclude || (exclude
-        && (trade.assets_given_desc !== 0 && trade.assets_received_desc.length !== 0))) {
-        let givenItems = '';
-        trade.assets_given_desc.forEach((item) => {
-          givenItems += `${item.market_hash_name}(${item.price.price}),`;
-        });
-        let receivedItems = '';
-        trade.assets_received_desc.forEach((item) => {
-          receivedItems += `${item.market_hash_name}(${item.price.price}),`;
-        });
-        lines += `${trade.steamid_other},${trade.partnerSummary.personaname},${trade.profitLoss},${trade.givenTotal},${trade.receivedTotal},"${givenItems}","${receivedItems}"\n`;
-      }
-    });
-    const encodedURI = `data:text/csv;charset=utf-8,${encodeURIComponent(csvContent + lines)}`;
-    setCsvContent(encodedURI);
-    setStatusMessage('Export finished, click the file icon to download the resulting .CSV file');
-    setShowDownload(true);
+    loadNextChunk(exportStartTimeUnix);
   };
 
   const onExcludeChange = (event) => {
@@ -39,11 +63,22 @@ const CSVExport = ({ trades }) => {
     setExclude(value);
   };
 
+  const onStartTimeChange = (date) => {
+    setExportStartTime(date);
+    const unixTimeStamp = parseInt(date.getTime() / 1000);
+    setExportStartTimeUnix(unixTimeStamp);
+  };
+
+  const onEndTimeChange = (date) => {
+    setExportEndTime(date);
+    const unixTimeStamp = parseInt(date.getTime() / 1000);
+    setExportEndTimeUnix(unixTimeStamp);
+  };
+
   return (
     <div className="trade-history__controls">
       <h3>Export to .CSV</h3>
       <div>
-        Config:
         <div>
           Exclude empty offers:&nbsp;
           <input
@@ -52,6 +87,12 @@ const CSVExport = ({ trades }) => {
             checked={exclude}
             title="Hide trades that are empty in one side"
           />
+          &nbsp;
+          After:&nbsp;
+          <DatePicker selected={exportStartTime} onChange={onStartTimeChange} />
+          &nbsp;
+          and before:&nbsp;
+          <DatePicker selected={exportEndTime} onChange={onEndTimeChange} />
         </div>
       </div>
       <div>
@@ -69,7 +110,7 @@ const CSVExport = ({ trades }) => {
       {
         showDownload
           ? (
-            <a href={csvContent} download="trade_history_export.csv" title="Download trade_history_export.csv">
+            <a href={downloadURI} download="trade_history_export.csv" title="Download trade_history_export.csv">
               <FontAwesomeIcon
                 icon={faFileExport}
               />
