ff32b768eab90cb3b90d89ea37cba044cb4fad98
unknown <gruczika@gmail.com>
2020-06-09 19:55:46 +0200

    trade-history-controls checkbox bug fixed, checkbox design, datepicker design created

--- extension/src/components/TradeHistory/CSVExport.js
+++ extension/src/components/TradeHistory/CSVExport.js
@@ -18,13 +18,19 @@ const CSVExport = () => {
   const [exportStartTimeUnix, setExportStartTimeUnix] = useState(
     parseInt(new Date().getTime() / 1000),
   );
-  const [exportEndTimeUnix, setExportEndTimeUnix] = useState(parseInt(new Date().getTime() / 1000));
+  const [exportEndTimeUnix, setExportEndTimeUnix] = useState(
+    parseInt(new Date().getTime() / 1000),
+  );
   const [requestCount, setRequestCount] = useState(0);
 
   const finishExport = () => {
-    const encodedURI = `data:text/csv;charset=utf-8,${encodeURIComponent(csvContent)}`;
+    const encodedURI = `data:text/csv;charset=utf-8,${encodeURIComponent(
+      csvContent,
+    )}`;
     setDownloadURI(encodedURI);
-    setStatusMessage('Export finished, click the file icon to download the resulting .CSV file');
+    setStatusMessage(
+      'Export finished, click the file icon to download the resulting .CSV file',
+    );
     setShowDownload(true);
   };
 
@@ -34,9 +40,13 @@ const CSVExport = () => {
     getTradeHistory(100, startTime, lastTradeID).then((tradesResponse) => {
       let lines = '';
       tradesResponse.trades.forEach((trade) => {
-        if ((!exclude || (exclude
-          && (trade.assets_given_desc !== 0 && trade.assets_received_desc.length !== 0)))
-          && exportEndTimeUnix >= trade.time_init) {
+        if (
+          (!exclude
+            || (exclude
+              && trade.assets_given_desc !== 0
+              && trade.assets_received_desc.length !== 0))
+          && exportEndTimeUnix >= trade.time_init
+        ) {
           let givenItems = '';
           trade.assets_given_desc.forEach((item) => {
             givenItems += `${item.market_hash_name}(${item.price.price}),`;
@@ -45,7 +55,11 @@ const CSVExport = () => {
           trade.assets_received_desc.forEach((item) => {
             receivedItems += `${item.market_hash_name}(${item.price.price}),`;
           });
-          lines += `${trade.steamid_other},${trade.partnerSummary.personaname},${new Date(trade.time_init * 1000)},${trade.profitLoss},${trade.givenTotal},${trade.receivedTotal},"${givenItems}","${receivedItems}"\n`;
+          lines += `${trade.steamid_other},${
+            trade.partnerSummary.personaname
+          },${new Date(trade.time_init * 1000)},${trade.profitLoss},${
+            trade.givenTotal
+          },${trade.receivedTotal},"${givenItems}","${receivedItems}"\n`;
         }
       });
       csvContent += lines;
@@ -59,7 +73,7 @@ const CSVExport = () => {
   };
 
   const onExcludeChange = (event) => {
-    const value = event.target.value;
+    const value = event.target.checked;
     setExclude(value);
   };
 
@@ -79,45 +93,65 @@ const CSVExport = () => {
     <div className="trade-history__controls">
       <h3>Export to .CSV</h3>
       <div>
-        <div>
-          Exclude empty offers:&nbsp;
-          <input
-            type="checkbox"
-            onChange={onExcludeChange}
-            checked={exclude}
-            title="Hide trades that are empty in one side"
-          />
-          &nbsp;
-          After:&nbsp;
-          <DatePicker selected={exportStartTime} onChange={onStartTimeChange} />
-          &nbsp;
-          and before:&nbsp;
-          <DatePicker selected={exportEndTime} onChange={onEndTimeChange} />
+        <div className="row">
+          <span className="trade-history__control">
+            <label className="checkmark">
+              Exclude empty offers:&nbsp;
+              <input
+                type="checkbox"
+                onChange={onExcludeChange}
+                checked={exclude}
+                title="Hide trades that are empty in one side"
+                className="checkmark__checkbox"
+              />
+              <span className="checkmark__designed" />
+            </label>
+          </span>
+        </div>
+        <div className="row">
+          <span className="trade-history__control">
+            After:&nbsp;
+            <DatePicker
+              selected={exportStartTime}
+              onChange={onStartTimeChange}
+              className="input"
+            />
+          </span>
+        </div>
+        <div className="row">
+          <span className="trade-history__control">
+            Before:&nbsp;
+            <DatePicker
+              selected={exportEndTime}
+              onChange={onEndTimeChange}
+              className="input"
+            />
+          </span>
         </div>
       </div>
-      <div>
-        {statusMessage}
+      <div>{statusMessage}</div>
+      <div className="row">
+        <span className="trade-history__control">
+          <CustomA11yButton
+            action={startExport}
+            title="Start export"
+            id="startExport"
+          >
+            Start export&nbsp;
+            <FontAwesomeIcon icon={faPlay} />
+          </CustomA11yButton>
+        </span>
       </div>
-      <CustomA11yButton
-        action={startExport}
-        title="Start export"
-        id="startExport"
-      >
-        <FontAwesomeIcon
-          icon={faPlay}
-        />
-      </CustomA11yButton>
-      {
-        showDownload
-          ? (
-            <a href={downloadURI} download="trade_history_export.csv" title="Download trade_history_export.csv">
-              <FontAwesomeIcon
-                icon={faFileExport}
-              />
-            </a>
-          )
-          : null
-      }
+
+      {showDownload ? (
+        <a
+          href={downloadURI}
+          download="trade_history_export.csv"
+          title="Download trade_history_export.csv"
+        >
+          <FontAwesomeIcon icon={faFileExport} />
+        </a>
+      ) : null}
     </div>
   );
 };
