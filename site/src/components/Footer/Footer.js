b94b81af8ef2f7d46d2fd2e8d4854eeead7a47b4
Gergely Szabo <gergely@gergely-szabo.com>
2020-07-30 19:06:53 +0200

    instagram -> twitter in footer

--- site/src/components/Footer/Footer.js
+++ site/src/components/Footer/Footer.js
@@ -20,7 +20,7 @@ const footer = () => {
                 <BrandIcon link='https://addons.mozilla.org/en-US/firefox/addon/csgo-trader-steam-trading/' brand='firefox'/>
                 <BrandIcon link='https://microsoftedge.microsoft.com/addons/detail/emcdnkamomgiafjejbhdpcfgbeeimpdb' brand='edge'/>
                 <BrandIcon link='https://www.youtube.com/channel/UCkDNavvHkCFHFCZ2bmG4zzw' brand='youtube'/>
-                <BrandIcon link='https://www.instagram.com/gerytrading/' brand='instagram'/>
+                <BrandIcon link='https://twitter.com/gergelyszabo94' brand='twitter'/>
             </div>
             <span className='footerNavLink'>
                 <Link to='/privacy/'>Privacy</Link>
