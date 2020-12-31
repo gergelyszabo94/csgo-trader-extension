30f7e682433170ef1046d0188e0caed7c9c83547
Gergely Szabo <gergely@gergely-szabo.com>
2020-12-31 19:40:04 +0100

    added credit to cantry

--- site/src/components/Changelog/Content/TwoDotTenDotOne.js
+++ site/src/components/Changelog/Content/TwoDotTenDotOne.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ChangelogInstance from '../ChangelogInstance';
+import NewTabLink from '../../NewTabLink/NewTabLink';
 
 const TwoDotTenDotOne = () => {
   return (
     <ChangelogInstance version="2.10.1" date="2020-12-31">
       <li>
         Added an option to load CS:GO inventories from an alternative endpoint.
+        (inspired by and with the help of <NewTabLink to="https://twitter.com/cantryde">cantry</NewTabLink>)
       </li>
       <li>
         Added retry to offer sending based on query params.
