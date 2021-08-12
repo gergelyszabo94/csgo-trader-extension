08fc2d071f39054d1fe47b57a27b24c3a6e814fa
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-08-12 15:43:00 -0400

    fix: remove unused param

--- backend/priceScraper/priceScraper.py
+++ backend/priceScraper/priceScraper.py
@@ -53,10 +53,10 @@ def lambda_handler(event, context):
         log.setLevel(logging.WARNING)
 
     own_prices, response = fetch_own_prices()
-    steam_prices = fetch_steamapis(response, stage)
-    csgobackpack_prices = fetch_csgobackpack(response)
-    bitskins_prices = request_bitskins(response, stage)
-    lootfarm_prices = request_lootfarm(response, stage)
+    steam_prices = fetch_steamapis(stage)
+    csgobackpack_prices = fetch_csgobackpack()
+    bitskins_prices = request_bitskins(stage)
+    lootfarm_prices = request_lootfarm(stage)
     csgotm_prices = fetch_csgotm(stage)
     csmoney_prices = request_csmoney(stage)
     skinport_prices = request_skinport(stage)
@@ -68,7 +68,7 @@ def lambda_handler(event, context):
     ) = request_priceempire(stage)
 
     # not used apparently
-    skinwallet_prices = fetch_skinwallet(response, stage)
+    skinwallet_prices = fetch_skinwallet(stage)
 
     csgotrader_prices = create_csgotrader_prices(buff163_prices, csgobackpack_prices, csmoney_prices, own_prices, stage, steam_prices)
 
@@ -294,7 +294,7 @@ def push_final_prices(bitskins_prices, buff163_prices, csgoempire_prices, csgoex
     push_to_s3(extract, 'prices_v6', stage)
 
 
-def fetch_skinwallet(response, stage) -> Dict[str, float]:
+def fetch_skinwallet(stage) -> Dict[str, float]:
     log.info("Requesting prices from skinwallet.com")
     try:
         response = requests.get("https://www.skinwallet.com/market/api/offers/overview", params={
@@ -530,7 +530,7 @@ def fetch_csgotm(stage) -> Dict[str, str]:
     return csgotm_prices
 
 
-def request_lootfarm(response, stage) -> Dict[str, float]:
+def request_lootfarm(stage) -> Dict[str, float]:
     log.info("Requesting prices from loot.farm")
 
     try:
@@ -570,7 +570,7 @@ def request_lootfarm(response, stage) -> Dict[str, float]:
     return lootfarm_prices
 
 
-def request_bitskins(response, stage) -> Dict[str, Dict[str, Optional[str]]]:
+def request_bitskins(stage) -> Dict[str, Dict[str, Optional[str]]]:
     log.info('Requesting bitskins prices')
 
     try:
@@ -612,7 +612,7 @@ def request_bitskins(response, stage) -> Dict[str, Dict[str, Optional[str]]]:
     return bitskins_prices
 
 
-def fetch_csgobackpack(response) -> Dict[str, Optional[Dict[str, Dict[str, Union[str, float]]]]]:
+def fetch_csgobackpack() -> Dict[str, Optional[Dict[str, Dict[str, Union[str, float]]]]]:
     log.info("Requesting prices from csgobackpack.net")
 
     try:
@@ -641,7 +641,7 @@ def fetch_csgobackpack(response) -> Dict[str, Optional[Dict[str, Dict[str, Union
     return csgobackpack_prices
 
 
-def fetch_steamapis(response, stage) -> Dict[str, Dict[str, float]]:
+def fetch_steamapis(stage) -> Dict[str, Dict[str, float]]:
     logging.info('Getting Prices from Steam APIs')
 
     try:
