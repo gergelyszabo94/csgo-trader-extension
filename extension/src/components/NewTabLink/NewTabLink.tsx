c1071acbb436442abc0b28a7ee68944b2f5f97b8
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-08-20 19:14:35 -0400

    fix: interface for `NewTabLink`

--- extension/src/components/NewTabLink/NewTabLink.tsx
+++ extension/src/components/NewTabLink/NewTabLink.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const newTabLink = ({
-  className, to, children,
-}) => {
+export interface newTabLinkProps {
+  to: string;
+  className?: string;
+  children: React.ReactNode
+}
+
+const newTabLink = ({className, to, children}: newTabLinkProps) => {
   return (
     <a href={to} target="_blank" rel="noopener" className={className}>
       {children}
