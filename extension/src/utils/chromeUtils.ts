2bef4c91f65687407d5450b224d97a99985f1bc9
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-08-23 11:24:05 -0400

    refactor: `chromeUtils`

---
+++ extension/src/utils/chromeUtils.ts
@@ -0,0 +1,45 @@
+// resolves the chrome.runtime api to use promises over callback functions
+// some functions already support this with overloading, but often times that overloading
+// becomes tedious whereas a utility function can abstract that hassle away from the developer.
+
+type keys = string | string[] | { [key: string]: any } | null;
+
+export const chromeStorageLocalGet = async (keys?: keys): Promise<{ [key: string]: any }> => {
+    if (keys) {
+        return await chrome.storage.local.get(keys);
+    }
+    // undefined | null
+    return new Promise((resolve) => {
+        chrome.storage.local.get(null, resolve);
+    });
+};
+
+export const chromeStorageLocalRemove = async (keys: string | string[]): Promise<void> => {
+    return new Promise<void>((resolve) => {
+        chrome.storage.local.remove(keys, resolve);
+    });
+};
+
+export const chromeRuntimeSendMessage = async (message: any): Promise<any> => {
+    return new Promise((resolve) => {
+        chrome.runtime.sendMessage(message, resolve);
+    });
+};
+
+export const chromeRuntimePlatformInfo = async (): Promise<chrome.runtime.PlatformInfo> => {
+    return new Promise((resolve) => {
+        chrome.runtime.getPlatformInfo(resolve);
+    });
+}
+
+// the next two don't actually do anything.
+// it's here mainly for name consisentcy with the others,
+// but they can be modified if it needs to be.
+
+export const chromeStorageLocalSet = async (items: { [key: string]: any }): Promise<void> => {
+    await chrome.storage.local.set(items);
+};
+
+export const chromeStorageLocalClear = async (): Promise<void> => {
+    await chrome.storage.local.clear();
+};
