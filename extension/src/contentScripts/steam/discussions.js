ef46e835b367fb186605c70ce764d972986d275c
Gergely Szabo <gergely@gergely-szabo.com>
2022-07-07 01:11:23 +0200

    fixed extension error on discussion search pages

--- extension/src/contentScripts/steam/discussions.js
+++ extension/src/contentScripts/steam/discussions.js
@@ -12,37 +12,40 @@ updateLoggedInUserInfo();
 addUpdatedRibbon();
 removeLinkFilterFromLinks();
 
-const nextBump = Math.floor((Math.random() * 10) + 31);
+// when searching with keyword  in the topic
+// it's a different kind of page where this is not applicable
+if (!window.location.href.includes('discussions/search/')) {
+  const nextBump = Math.floor((Math.random() * 10) + 31);
 
-const forumTopicEl = document.querySelector('.commentthread_area.forumtopic_comments');
-const abuseID = forumTopicEl.id.split('commentthread_ForumTopic_')[1].split('_')[0];
-const gIDForum = forumTopicEl.id.split('_')[3];
-const gIDTopic = forumTopicEl.id.split('_')[4];
+  const forumTopicEl = document.querySelector('.commentthread_area.forumtopic_comments');
+  const abuseID = forumTopicEl.id.split('commentthread_ForumTopic_')[1].split('_')[0];
+  const gIDForum = forumTopicEl.id.split('_')[3];
+  const gIDTopic = forumTopicEl.id.split('_')[4];
 
-const doTheAutoBumping = () => {
-  const getExtendedDataScript = `document.querySelector('body').setAttribute('commentExtendedData', g_rgCommentThreads['${forumTopicEl.id.split('commentthread_')[1].split('_area')[0]}'].m_rgCommentData.extended_data)`;
-  const extendedData = injectScript(getExtendedDataScript, true, 'getExtendedData', 'commentExtendedData');
+  const doTheAutoBumping = () => {
+    const getExtendedDataScript = `document.querySelector('body').setAttribute('commentExtendedData', g_rgCommentThreads['${forumTopicEl.id.split('commentthread_')[1].split('_area')[0]}'].m_rgCommentData.extended_data)`;
+    const extendedData = injectScript(getExtendedDataScript, true, 'getExtendedData', 'commentExtendedData');
 
-  document.querySelectorAll('.commentthread_comment').forEach((commentThread) => {
-    const commentTextElement = commentThread.querySelector('.commentthread_comment_text');
-    if (commentTextElement !== null && commentTextElement.innerText.includes('Bump')
+    document.querySelectorAll('.commentthread_comment').forEach((commentThread) => {
+      const commentTextElement = commentThread.querySelector('.commentthread_comment_text');
+      if (commentTextElement !== null && commentTextElement.innerText.includes('Bump')
       && !commentThread.classList.contains('commentthread_deleted_comment')
       && !commentThread.classList.contains('commentthread_deleted_expanded')) {
-      const commentID = commentThread.id.split('comment_')[1];
-      deleteForumComment(abuseID, gIDForum, gIDTopic, commentID, extendedData);
-      commentTextElement.innerText = `CSGO Trader replaced this Bump comment with a new one. The page will refresh and the next bump will happen in ${nextBump} minutes.`;
-    }
-  });
+        const commentID = commentThread.id.split('comment_')[1];
+        deleteForumComment(abuseID, gIDForum, gIDTopic, commentID, extendedData);
+        commentTextElement.innerText = `CSGO Trader replaced this Bump comment with a new one. The page will refresh and the next bump will happen in ${nextBump} minutes.`;
+      }
+    });
 
-  postForumComment(abuseID, gIDForum, gIDTopic, 'Bump', extendedData);
-};
+    postForumComment(abuseID, gIDForum, gIDTopic, 'Bump', extendedData);
+  };
 
-const searchElement = document.getElementById('DiscussionSearchForm');
-if (searchElement !== null) {
-  searchElement.insertAdjacentHTML(
-    'afterend',
-    DOMPurify.sanitize(
-      `<div class="discussionAutoBump">
+  const searchElement = document.getElementById('DiscussionSearchForm');
+  if (searchElement !== null) {
+    searchElement.insertAdjacentHTML(
+      'afterend',
+      DOMPurify.sanitize(
+        `<div class="discussionAutoBump">
                 CSGOTrader Auto-bump this post:
                 <input
                     type="checkbox"
@@ -51,37 +54,38 @@ if (searchElement !== null) {
                     making the post appear on top of discussions. The post has to be kept open on a page to be auto-bumped."
                 />
             </div>`,
-    ),
-  );
-  const autoBumpCheckBox = document.getElementById('autoBumpDiscussion');
+      ),
+    );
+    const autoBumpCheckBox = document.getElementById('autoBumpDiscussion');
 
-  chrome.storage.local.get(['discussionsToAutoBump'], ({ discussionsToAutoBump }) => {
-    if (discussionsToAutoBump.includes(window.location.href)) {
-      autoBumpCheckBox.checked = true;
-      doTheAutoBumping();
+    chrome.storage.local.get(['discussionsToAutoBump'], ({ discussionsToAutoBump }) => {
+      if (discussionsToAutoBump.includes(window.location.href)) {
+        autoBumpCheckBox.checked = true;
+        doTheAutoBumping();
 
-      setTimeout(() => {
-        window.location.reload();
-      }, nextBump * 60 * 1000);
-    } else autoBumpCheckBox.checked = false;
+        setTimeout(() => {
+          window.location.reload();
+        }, nextBump * 60 * 1000);
+      } else autoBumpCheckBox.checked = false;
 
-    autoBumpCheckBox.addEventListener('change', (event) => {
-      let newDiscussionsToAutoBump = [];
-      if (event.target.checked) {
-        if (discussionsToAutoBump.includes(window.location.href)) {
-          newDiscussionsToAutoBump = discussionsToAutoBump;
-        } else newDiscussionsToAutoBump = [...discussionsToAutoBump, window.location.href];
-      } else if (discussionsToAutoBump.includes(window.location.href)) {
-        newDiscussionsToAutoBump = discussionsToAutoBump.filter((href) => {
-          return href !== window.location.href;
+      autoBumpCheckBox.addEventListener('change', (event) => {
+        let newDiscussionsToAutoBump = [];
+        if (event.target.checked) {
+          if (discussionsToAutoBump.includes(window.location.href)) {
+            newDiscussionsToAutoBump = discussionsToAutoBump;
+          } else newDiscussionsToAutoBump = [...discussionsToAutoBump, window.location.href];
+        } else if (discussionsToAutoBump.includes(window.location.href)) {
+          newDiscussionsToAutoBump = discussionsToAutoBump.filter((href) => {
+            return href !== window.location.href;
+          });
+        } else newDiscussionsToAutoBump = discussionsToAutoBump;
+
+        chrome.storage.local.set({ discussionsToAutoBump: newDiscussionsToAutoBump }, () => {
+          window.location.reload();
         });
-      } else newDiscussionsToAutoBump = discussionsToAutoBump;
-
-      chrome.storage.local.set({ discussionsToAutoBump: newDiscussionsToAutoBump }, () => {
-        window.location.reload();
       });
     });
-  });
 
-  overrideShowTradeOffer();
+    overrideShowTradeOffer();
+  }
 }
