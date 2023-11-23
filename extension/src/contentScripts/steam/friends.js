a86e3785bfea1d72a0b3758ed28c8725c5bd7612
Gergely Szabo <gergely@gergely-szabo.com>
2023-11-24 00:19:53 +0100

    removed goldening

--- extension/src/contentScripts/steam/friends.js
+++ extension/src/contentScripts/steam/friends.js
@@ -10,17 +10,6 @@ addUpdatedRibbon();
 listenToAcceptTrade();
 reloadPageOnExtensionUpdate();
 
-// highlights profiles with "csgotrader.app" in their name with gold colors
-document.querySelectorAll('.selectable.friend_block_v2.persona').forEach((friendBlock) => {
-  if (friendBlock.querySelector('.friend_block_content').innerText.toLowerCase().includes('csgotrader.app')) {
-    const avatar = friendBlock.querySelector('.player_avatar');
-    avatar.classList.remove('online', 'offline', 'in-game');
-    avatar.classList.add('golden');
-    friendBlock.classList.remove('online', 'offline', 'in-game');
-    friendBlock.classList.add('golden');
-  }
-});
-
 // adds links to friends and invites options
 const friendTitle = document.querySelector('.profile_friends.title_bar');
 if (friendTitle !== null) {
