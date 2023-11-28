0105aa215d5670c24e9b26ed115d778a8f0cb3ae
Gergely Szabo <gergely@gergely-szabo.com>
2023-11-28 22:47:03 +0100

    removed old, unused method

--- extension/src/utils/injection.js
+++ extension/src/utils/injection.js
@@ -3,18 +3,9 @@
 // using method 1 and 3 from https://stackoverflow.com/a/9517879/3862289
 
 const injectScript = (scriptString, toRemove, id, executeAndReturn) => {
-  // removes previously added instance of the script
-  const elementFromBefore = document.getElementById(id);
-  if (elementFromBefore !== null) elementFromBefore.remove();
-
-  const toInject = document.createElement('script');
-  toInject.id = id;
-
   document.documentElement.setAttribute('onreset', scriptString);
   document.documentElement.dispatchEvent(new CustomEvent('reset'));
   document.documentElement.removeAttribute('onreset');
-
-  (document.head || document.documentElement).appendChild(toInject);
 
   const simpleAttributeParsing = ['steamidOfLoggedinUser', 'steamidOfProfileOwner', 'tradePartnerSteamID', 'inventoryOwnerID', 'listingsInfo',
     'inventoryInfo', 'allItemsLoaded', 'offerInventoryInfo', 'steamWalletCurrency', 'steamWallet', 'formattedToInt', 'intToFormatted',
@@ -24,8 +15,6 @@ const injectScript = (scriptString, toRemove, id, executeAndReturn) => {
     ? document.querySelector('body').getAttribute(executeAndReturn)
     : null;
   if (result !== null) document.querySelector('body').removeAttribute(executeAndReturn);
-
-  if (toRemove) document.head.removeChild(toInject);
   return result;
 };
 
