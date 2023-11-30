cc1dba984f7d3f567fc86299e73114c3b014fe9b
Gergely Szabo <gergely@gergely-szabo.com>
2023-11-30 23:25:37 +0100

    removed bitskins p2p

--- extension/src/contentScripts/steam/marketListing.js
+++ extension/src/contentScripts/steam/marketListing.js
@@ -1376,13 +1376,6 @@ chrome.storage.local.get(['showRealMoneySiteLinks'], ({ showRealMoneySiteLinks }
                     </a>
                   </div>
                   <div class="realMoneySite">
-                    <a href="https://p2p.bitskins.com/affiliate#gery" target="_blank" class="realMoneySiteLink referralLink" data-site="bitskinsP2P">
-                        <img alt="Bitskins logo" style="height: 50px" src="${chrome.runtime.getURL('images/external_logos/bitskins.png')}" data-site="bitskinsP2P">
-                        <br>
-                        p2p.bitskins.com
-                    </a>
-                  </div>
-                  <div class="realMoneySite">
                     <a href="https://skinbid.com/?ref=csgotrader" target="_blank" class="realMoneySiteLink referralLink" data-site="skinbid">
                         <img alt="SkinBid logo" style="height: 50px" src="${chrome.runtime.getURL('images/external_logos/skinbid.png')}" data-site="skinbid">
                         <br>
@@ -1401,7 +1394,6 @@ chrome.storage.local.get(['showRealMoneySiteLinks'], ({ showRealMoneySiteLinks }
                       <a href="https://gamerpay.gg/partner/okn9go3r" target="_blank" class="referralLink" data-site="gamerpay">GamerPay</a>,
                       <a href="https://waxpeer.com/r/gery" target="_blank" class="referralLink" data-site="waxpeer">Waxpeer</a>,
                       <a href="https://buff.market/r/U1093423730" target="_blank" class="referralLink" data-site="buffmarket">BUFF.MARKET</a>,
-                      <a href="https://p2p.bitskins.com/affiliate#gery" target="_blank" class="referralLink" data-site="bitskinsP2P">p2p.bitskins.com</a>,
                       <a href="bskn.co/?ref_alias=xcW4c_phcUc" target="_blank" class="referralLink" data-site="bitskins">Bitskins</a> and 
                       <a href="https://csfloat.com?ref=gerytrading" target="_blank" class="referralLink" data-site="csfloat">
                       CSFloat
