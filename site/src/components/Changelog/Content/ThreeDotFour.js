0b2664ce0327e61055a9142f538d7ca8edd6e13b
Gergely Szabo <gergely@gergely-szabo.com>
2025-07-22 13:29:07 +0200

    release 3.4

---
+++ site/src/components/Changelog/Content/ThreeDotFour.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ChangelogInstance from '../ChangelogInstance';
+import GithubIssueLink from '../GithubIssueLink';
+
+const ThreeDotFour = () => {
+  return (
+    <ChangelogInstance version="3.4" date="2025-07-22">
+      <li>
+        Added support for Trade Protected items
+      </li>
+      <li>
+        Added option to resize and reposition the Trade Protection icon in one's own inventory
+      </li>
+      <li>
+        Added links to incoming trade offers and a summary and ability to cancel sent offers when the trade offers page can't be loaded
+      </li>
+      <li>
+        Fixed inventory totals only showing non-trade protected items
+      </li>
+    </ChangelogInstance>
+  );
+}
+
+export default ThreeDotFour;
--- site/src/containers/Changelog/Changelog.js
+++ site/src/containers/Changelog/Changelog.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 
 import Head from '../../components/Head/Head';
 
+import ThreeDotFour from '../../components/Changelog/Content/ThreeDotFour.js';
 import ThreeDotThreeDotThree from '../../components/Changelog/Content/ThreeDotThreeDotThree.js';
 import ThreeDotThreeDotTwo from '../../components/Changelog/Content/ThreeDotThreeDotTwo.js';
 import ThreeDotThreeDotOne from '../../components/Changelog/Content/ThreeDotThreeDotOne.js';
@@ -111,6 +112,7 @@ const changelog = (props) => {
                 The platforms (Chrome Web Store, Mozilla Addons, Edge Add-ons) review submissions before they are available for download.
                 This means that the latest version might not have rolled out to your platform yet when you are reading this.
             </Container>
+            <ThreeDotFour />
             <ThreeDotThreeDotThree />
             <ThreeDotThreeDotTwo />
             <ThreeDotThreeDotOne />
