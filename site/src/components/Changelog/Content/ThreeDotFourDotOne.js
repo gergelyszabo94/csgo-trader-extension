cc5914eeed5fd61c0e3b99c884ee7ceea37028ed
Gergely Szabo <gergely@gergely-szabo.com>
2025-08-22 23:18:48 +0200

    release 3.4.1

---
+++ site/src/components/Changelog/Content/ThreeDotFourDotOne.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ChangelogInstance from '../ChangelogInstance';
+
+const ThreeDotFourDotOne = () => {
+  return (
+    <ChangelogInstance version="3.4.1" date="2025-08-22">
+      <li>
+        Added support for Souvenir Charms
+      </li>
+      <li>
+        Fixed lots of bugs introduced in the previous version
+      </li>
+      <li>
+        Added iTrade.gg affiliate link
+      </li>
+      <li>
+        Removed csgoexo and skinwallet as pricing providers
+      </li>
+    </ChangelogInstance>
+  );
+}
+
+export default ThreeDotFourDotOne;
